b4c3295ad3370b2749e0586dffff584769ae3b3a
obastosleandro <bastosleandrodesigner@gmail.com>
2021-08-31 16:26:41 -0300

    corrigindo a questao 2.

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,9 @@ function compareTrue(v1, v2) {
 
 // Desafio 2
 function calcArea(base, height) {
-let area = (base * height) /2;
-} return area;
+let area = (base * height) / 2;
+ return area;
+}
 
 // Desafio 3
 function splitSentence() {
