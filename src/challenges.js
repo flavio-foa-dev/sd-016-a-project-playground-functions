dd623bfa66dfd3e2642d56a39265a3e633ce5e5a
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-08-31 19:05:33 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -32,16 +32,16 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount(array) {
-  let highest = -100;
+function highestCount(arr) {
+  let highest = arr[0];
   let times = 0;
-  for (let i = 0; i < array.length; i += 1){
-    if (array[i] > highest){
-      highest = array[i];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > highest) {
+      highest = arr[i];
     }
   }
-  for (let i = 0; i < array.length; i += 1){
-    if (array[i] === highest){
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === highest) {
       times += 1;
     }
   }
@@ -49,13 +49,30 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let mouseCat1Pos = mouse - cat1;
+  let mouseCat2Pos = mouse - cat2;
+  
+  if (mouseCat1Pos < 0) {
+    mouseCat1Pos = mouseCat1Pos * -1;
+  }
+  if (mouseCat2Pos < 0) {
+    mouseCat2Pos = mouseCat2Pos * -1;
+  } 
+  if (mouseCat1Pos < mouseCat2Pos) {
+    return 'cat1'
+  } else if ( mouseCat1Pos > mouseCat2Pos) {
+    return 'cat2'
+  } else {
+    return 'os gatos trombam e o rato foge'
+  }
 }
+console.log(catAndMouse(1, 0, 2))
+
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrNumber) {
+  let arrFB
 }
 
 // Desafio 9
