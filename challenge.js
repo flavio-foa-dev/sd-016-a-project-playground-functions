c445d5bd851067b1a87f5e32a1bd7c84276a99e8
Kazuo Abduch <kazuo.abduch@gmail.com>
2021-08-31 18:34:30 -0300

    Commit Desafios 1-10

---
+++ challenge.js
@@ -0,0 +1,164 @@
+//Desafio 1
+function compareTrue(value1, value2){
+
+    if(value1 == true && value2 == true){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+//Desafio 2
+function calcArea (base, height){
+    return base*height/2;
+
+}
+
+//Desafio 3
+function splitSentence(texto){
+   return texto.split(' ');
+
+}
+
+//Desafio 4
+function concatName(array){
+    return array.join(',');
+
+}
+
+//Desafio 5
+function footballPoints(wins, ties){
+    let pointsWin = wins*3;
+    let pointTies = ties;
+
+    return pointTies + pointsWin;
+
+}
+
+//Desafio 6
+function highestCount(array){
+
+    let count = 0;
+
+    for(let indexCount = 0 ; indexCount < array.length ; indexCount++){
+        
+        for(let indexCurrent = 0 ; indexCurrent < array.length ; indexCurrent++){
+            
+            if(array[indexCount] > array[indexCurrent]){
+
+            let currentIndex = array[indexCount];
+            array[indexCount] = array[indexCurrent];
+            array[indexCurrent] = currentIndex;
+            
+            }
+        }
+    }
+    for (let repCount = 0 ; repCount < array.length ; repCount++){
+
+        if (array[0] == array[repCount]){
+            count++;
+        }
+    }
+    
+    return count;
+
+}
+
+//Desafio 7
+function catAndMouse(mouse, cat1, cat2){
+    if(cat1 - mouse > cat2 - mouse){
+        return "cat1";
+
+    }else if (cat2 - mouse > cat1 - mouse){
+        return "cat2"
+    }else {
+        return "os gatos trombam e o rato foge"
+    }
+
+}
+
+//Desafio 8
+function fizzBuzz(array){
+    let arrayResposta = [];
+
+    for(let count = 0 ; count < array.length ; count++){
+        if(array[count]%3 == 0 && array[count]%5 != 0){
+            arrayResposta.push('fizz');
+        }else if (array[count]%5 == 0 && array[count]%3 != 0){
+            arrayResposta.push('buzz');
+        }else if (array[count]%3 == 0 && array[count]%5 == 0){
+            arrayResposta.push('fizzBuzz');
+        }else {
+            arrayResposta.push('bug');
+        }
+
+    }
+    return arrayResposta;
+
+}
+
+//Desafio 9
+let texto = "hi there!";
+function encode(texto){
+    let textoArray = texto.split('')
+    for (index = 0 ; index < textoArray.length ; index++){
+        switch (textoArray[index]){
+            case 'a': 
+                textoArray[index] = '1';
+                break;
+            case 'e': 
+                textoArray[index] = '2';
+                break;
+            case 'i': 
+                textoArray[index] = '3';
+                break;
+            case 'o': 
+                textoArray[index] = '4';
+                break;
+            case 'u': 
+                textoArray[index] = '5';
+                break;
+        }
+    }  
+    return textoArray.join('');
+}
+let textocod = encode(texto);
+function decode(textocod){
+    let textoArray = textocod.split('')
+    for (index = 0 ; index < textoArray.length ; index++){
+        switch (textoArray[index]){
+            case '1': 
+                textoArray[index] = 'a';
+                break;
+            case '2': 
+                textoArray[index] = 'e';
+                break;
+            case '3': 
+                textoArray[index] = 'i';
+                break;
+            case '4': 
+                textoArray[index] = 'o';
+                break;
+            case '5': 
+                textoArray[index] = 'u';
+                break;
+        }
+    }
+    return textoArray.join('');
+}
+
+//Desafio 10
+function ordena(array, name){
+            
+    if(array.length === 0){
+        return 'Vazio!';
+
+    }else{
+        array.sort();
+        let arrayOrdenada = [];
+        for(let index = 0 ; index < array.length ; index++){
+            arrayOrdenada.push({'tech':array[index], 'name': name})
+        }
+        return arrayOrdenada;
+    }
+}
