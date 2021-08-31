acd7014527a8aa78e86a568fce2b607dc2962042
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-08-31 16:43:49 -0300

    Completa exercicios ate o 07.

--- src/challenges.js
+++ src/challenges.js
@@ -34,18 +34,52 @@ function concatName(arrayOfStrings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+// Escreva uma função com o nome footballPoints que receba o número de vitórias (esse parâmetro deverá se chamar wins)
+// e o número de empates (esse parâmetro deverá se chamar ties) e retorne a quantidade de pontos que o time marcou em um campeonato.
+// Para tanto, considere que cada vitória vale 3 pontos e cada empate vale 1 ponto.
+function footballPoints(wins, ties) {
+  let winPoints = wins * 3;
+  let tiesPoints = ties;
+  return winPoints + tiesPoints;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+// Escreva uma função chamada highestCount que, ao receber uma array de números,
+// retorne a quantidade de vezes que o maior deles se repete.
+
+function highestCount(arrayOfNumbers) {
+  let sortedNumbers = arrayOfNumbers.sort((a,b) => b-a);
+  let theFirst = sortedNumbers[0];
+  let count = 1;
+
+  for (let index = 1; index < sortedNumbers.length; index++) {
+    if (theFirst === sortedNumbers[index]) {
+      count += 1;
+    } else {
+      break
+    }
+  }
+  return count;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Imagine que existem dois gatos, os quais chamaremos de cat1 e cat2,
+// e que ambos estão caçando um mesmo rato chamado mouse.
+// Imagine que os animais estão em uma reta, cada um em uma posição representada por um número.
+// Sabendo disso, crie uma função chamada catAndMouse que,
+// ao receber a posição de mouse, cat1 e cat2, nessa ordem, calcule as distâncias entre o rato e cada um dos gatos,
+// em seguida, retorne qual dos felinos irá alcançar o rato primeiro (aquele que estiver mais perto do rato).
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceCat1 = Math.abs(mouse - cat1);
+  let distanceCat2 = Math.abs(mouse - cat2);
+
+  if (distanceCat1 === distanceCat2) {
+    return "os gatos trombam e o rato foge";
+  } else if (distanceCat1 < distanceCat2) {
+    return "cat1";
+  } else {
+    return "cat2";
+  }
 }
 
 // Desafio 8
