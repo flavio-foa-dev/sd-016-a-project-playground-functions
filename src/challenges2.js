c122a6060566f2d2ee2d890aa350d61ed73d8577
arctavares <cosilk@hotmail.com>
2021-08-31 16:44:12 -0300

    Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -44,7 +44,6 @@ function generatePhoneNumber(array) {
         phoneNumber.push(array[i]);
       }
     }
-
   }
 
   for(let i = 0; i < array.length; i += 1){
@@ -69,8 +68,12 @@ function generatePhoneNumber(array) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if(((lineA < lineB+lineC) && (lineB < lineA + lineC) && (lineC < lineA + lineB)) && ((lineA > Math.abs(lineB - lineC) && (lineB > Math.abs(lineA - lineC) && (lineC > Math.abs(lineA - lineB)))))){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 // Desafio 13
