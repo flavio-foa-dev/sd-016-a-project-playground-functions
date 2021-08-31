0693f545911d4ba1ca31874394d97aaeb1a77cb9
severo <oliveira.severo@gmail.com>
2021-08-31 17:35:46 -0300

    [🤯  Desafios 1 e 2 ] [Requisitos atentidos]

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,18 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui
   // meu primeiro 
+  if (value1 === true && value2 === true ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
