de50c3f79bc97e9f2897215a85a7404b1e1b538f
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-08-31 15:05:40 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -15,9 +15,11 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+    let array = string.split([' '])
+  return array;
 }
+console.log(splitSentence('go trybe'))
 
 // Desafio 4
 function concatName() {
