fa732a28483724f7a8f69172b7e018da84e215ca
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2021-08-31 15:00:19 -0300

    Desafio um if/else

--- src/challenges.js
+++ src/challenges.js
@@ -1,8 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true){
+    return true;
+  }else{
+    return false;
+  }
 }
-
+console.log(compareTrue(true, true));
 // Desafio 2
 function calcArea() {
   // seu código aqui
