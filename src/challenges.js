b252e308014065153b5c95e8edb2b92f951952cc
andreluis29 <andre.wenningkamp1@gmail.com>
2021-08-31 17:02:34 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -12,9 +12,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(sentence) {
+ return sentence.split(" ");
 }
+
 
 // Desafio 4
 function concatName() {
