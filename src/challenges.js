ceec5b314acda56e92e234c33f56719c42ad7281
alt-art <pedromendescraft@gmail.com>
2021-08-31 15:45:36 -0300

    Challenge 7

--- src/challenges.js
+++ src/challenges.js
@@ -38,8 +38,16 @@ function highestCount(array) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Distance = Math.abs(mouse - cat1);
+  let cat2Distance = Math.abs(mouse - cat2);
+  if (cat1Distance === cat2Distance) {
+    return 'os gatos trombam e o rato foge';
+  }
+  if (cat1Distance < cat2Distance) {
+    return 'cat1';
+  }
+  return 'cat2';
 }
 
 // Desafio 8
