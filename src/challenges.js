0c27841088836db3a60a822a03595f24f2429cfb
Lonardow <lonardow_fg@hotmail.com>
2021-08-31 14:52:20 -0300

    desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,18 @@
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  if (valor1 === true && valor2 === true){
-  return true;
+  if (valor1 === true && valor2 === true) {
+    return true;
   } else {
-  return false;
+    return false;
   }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
 }
-
-// Desafio 3
+// Desafio 3n
 function splitSentence() {
   // seu código aqui
 }
