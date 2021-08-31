ba85aa7c9f2959cef9167c6151246ea66c87a0eb
FelipeVahia <felipevahia@gmail.com>
2021-08-31 15:30:12 -0300

    exercicio7

--- src/challenges.js
+++ src/challenges.js
@@ -49,8 +49,17 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distance1 = Math.abs(mouse - cat1);
+  let distance2 = Math.abs(mouse - cat2);
+  if (distance1 > distance2) {
+    return 'cat2';
+  }
+  if (distance1 < distance2) {
+    return 'cat1';
+  }
+
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
