11b918cc28c8844453521064ee1c984ab6ddcfe1
Igor <iigorfelipe@gmail.com>
2021-08-31 19:15:52 -0300

    finaliza mais desafios

--- src/challenges.js
+++ src/challenges.js
@@ -1,35 +1,40 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if (value1 && value2 === true){
-    return true
+  if (value1 && value2 === true) {
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(strings) {
-  return array = strings.split(' ')  
+  return strings.split(' ');
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+ return array[array.length-1] + ', ' + array[0];
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+// Desafio 5 
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let bigger = Math.max(...array);
+  let times = Number();
+  for (let i in array) {
+   array[i] === bigger ? times += 1 : '';
+  }
+  return times;
 }
 
 // Desafio 7
