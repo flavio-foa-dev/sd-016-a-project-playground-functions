2ab77e433cb381042f3ce12dcfcc3d9dd82f308b
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-31 17:30:51 -0300

    requisito 8 comentado

--- src/challenges.js
+++ src/challenges.js
@@ -81,10 +81,33 @@ function catAndMouse(mouse,cat1,cat2) {
   }
 
 }
+console.log(catAndMouse(2,8,3))
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  const fizzBuzz = [];
+          /* crieia funçao com o parametro "array", criei uma constante 
+          chamada fizzBuzz com valor de um array vazio "[]" */
+  for (let index of array) {
+            /* inicio do laco de repetição começando 
+           com o indice valendo o primeiro valor do array */
+    if (index % 5 === 0 && index % 3 === 0){
+      fizzBuzz.push('fizzBuzz');/* se o resto '%' da divisao
+          do index ="2" por 5 for === igual a 0, '&&' 'e' o resto da do index = 2 por 3 for 
+          igual === 0; o fizzbuzz.push ira inserir a string  'fizzbuzz' dentro do array fizzBuzz[]*/
+    } else if (index % 3 === 0){
+      fizzBuzz.push('fizz');/* senao se o resto da % divisao do index =2 por 3 === 0; 
+                              o fizzBuzz.push ira inserir a string "fizz" no array "fizzBuzz[]" */
+    } else if (index % 5 === 0){
+      fizzBuzz.push('buzz');/* senao se o resto % da divisao do index =2 por 5 === 0; 
+                               o fizzBuzz.push ira inserir a string "buzz" no array "fizzBuzz[]" */
+    } else {
+      fizzBuzz.push('bug!');/* se nao for nenhumas das altenativas acima significa
+                             que o numero nao é divisivel po 3 ou 5 e entao o fizzBuzz.push ira inserir "bug" no array "fizzBuzz" */
+    }
+  }
+  return fizzBuzz;/* no final retornar o que aconteceu para dentro do array fizzBuzz */
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]));/* aqui esta o parametro que é um array */
 
 // Desafio 9
 function encode() {
