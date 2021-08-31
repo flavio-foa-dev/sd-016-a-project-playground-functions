9062204c672accfaf76f04a7eefe21a0e7a3ef07
Ryogazz <josuex.x.x@hotmail.com>
2021-08-31 18:16:47 -0300

    desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -25,41 +25,49 @@ function concatName(array) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return((wins * 3) + (ties * 1))
+  return ((wins * 3) + (ties * 1))
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+function highestCount(numeros) {
+  let cont = Math.max(...numeros);
+  let quant = 0;
+  for (let numero in numeros) {
+    if(numeros[numero] === cont){
+      quant ++;
+    }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+    }
+    return quant;
+  }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+  // Desafio 7
+  function catAndMouse() {
+    // seu código aqui
+  }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+  // Desafio 8
+  function fizzBuzz() {
+    // seu código aqui
+  }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-};
+  // Desafio 9
+  function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
+
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
+  };
