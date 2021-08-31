ca70b505f66e6effdf8dc919c0a18a2cb3683e68
Filipe Ferreira Paulo <filipef.paulo@gmail.com>
2021-08-31 18:55:42 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,27 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]))
+
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return("Array com tamanho incorreto.")
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return("não é possível gerar um número de telefone com esses valores");
+    }
+    let sum = 0
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
+      if (array[index] === array[index2]) {
+        sum += 1
+        if (sum >= 3) {
+          return("não é possível gerar um número de telefone com esses valores");
+        }
+      }
+    }
+  }
+  return('(' + array[0] + array[1] + ') ' + array[2]+ array[3]+ array[4]+ array[5]+ array[6] + '-' + array[7] + array[8] + array[9] + array[10])
 }
 
 // Desafio 12
