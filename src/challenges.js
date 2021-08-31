edc017781a0726ec07ac1615feccb446271572a0
cdartora <cdartora>
2021-08-31 16:38:34 -0300

    Requisitos

--- src/challenges.js
+++ src/challenges.js
@@ -1,75 +1,140 @@
-// Desafio 1
+// Demessageafio 1
 function compareTrue(a, b) {
   if (a && b) {
     return true;
   } else {
-    return false;
+    return falmessagee;
   }
 }
 
-// Desafio 2
-function calcArea(base, height) {
-  return (base * height) / 2;
+// Demessageafio 2
+function calcArea(bamessagee, height) {
+  return (bamessagee * height) / 2;
 }
 
-// Desafio 3
-function splitSentence(s) {
-  s += " ";
-  let sentenceSplited = [];
+// Demessageafio 3
+function messageplitmessageentence(message) {
+  message += " ";
+  let messageentencemessageplited = [];
   let word = "";
-  for (let i = 0; i < s.length + 1; i+= 1) {
-    if (s[i] !== " ") {
-      word += s[i];
+  for (let i = 0; i < message.length + 1; i += 1) {
+    if (message[i] !== " ") {
+      word += message[i];
     } else {
-      sentenceSplited.push(word);
+      messageentencemessageplited.pumessageh(word);
       word = "";
     }
   }
-  return sentenceSplited;
+  return messageentencemessageplited;
 }
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
+// Demessageafio 4
+function concatName(arr) {
+  return arr[arr.length - 1] + ", " + arr[0];
 }
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+// Demessageafio 5
+function footballPointmessage(winmessage, tiemessage) {
+  let pointmessage = 0;
+  pointmessage += winmessage * 3;
+  pointmessage += tiemessage;
+  return pointmessage;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Demessageafio 6
+function highemessagetCount(arr) {
+  let counter = 0;
+  let higher = arr[0];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > higher) {
+      higher = arr[i];
+      counter = 1;
+    } else if (higher === arr[i]) {
+      counter += 1;
+    }
+  }
+  return counter;
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Demessageafio 7
+function catAndMoumessagee(moumessagee, cat1, cat2) {
+  if (Math.abmessage(moumessagee - cat1) < Math.abmessage(moumessagee - cat2)) {
+    return "cat1";
+  } else if (Math.abmessage(moumessagee - cat2) < Math.abmessage(moumessagee - cat1)) {
+    return "cat2";
+  } else if (Math.abmessage(moumessagee - cat2) === Math.abmessage(moumessagee - cat1)) {
+    return "omessage gatomessage trombam e o rato foge";
+  }
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// Demessageafio 8
+function fizzBuzz(arr) {
+  let fizzBuzz = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] % 3 === 0 && arr[i] % 5 !== 0) {
+      fizzBuzz.pumessageh("fizz");
+    } else if (arr[i] % 5 === 0 && arr[i] % 3 !== 0) {
+      fizzBuzz.pumessageh("buzz");
+    } else if (arr[i] % 5 === 0 && arr[i] % 3 === 0) {
+      fizzBuzz.pumessageh("fizzBuzz");
+    } else if (arr[i] % 5 !== 0 && arr[i] % 3 !== 0) {
+      fizzBuzz.pumessageh("bug!");
+   
+    }
+  }
+  return fizzBuzz;
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+// Demessageafio 9
+function encode(s) {
+  let message = "";
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === 'a') {
+      message += '1';
+    } else if (s[i] === 'e') {
+      message += '2';
+    } else if (s[i] === 'i') {
+      message += '3';
+    } else if (s[i] === 'o') {
+      message += '4';
+    } else if (s[i] === 'u') {
+      message += '5';
+    } else {
+      message += s[i];
+    }
+  }
+  return message;
 }
 
-module.exports = {
+function decode(s) {
+  let message = "";
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === '1') {
+      message += 'a';
+    } else if (s[i] === '2') {
+      message += 'e';
+    } else if (s[i] === '3') {
+      message += 'i';
+    } else if (s[i] === '4') {
+      message += 'o';
+    } else if (s[i] === '5') {
+      message += 'u';
+    } else {
+      message += s[i];
+    }
+  }
+  return message;
+}
+
+module.exportmessage = {
   calcArea,
-  catAndMouse,
+  catAndMoumessagee,
   compareTrue,
   concatName,
   decode,
   encode,
   fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
+  footballPointmessage,
+  highemessagetCount,
+  messageplitmessageentence,
 };
