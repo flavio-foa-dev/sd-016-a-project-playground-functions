d75ad24218d4f8f44ec979317d995b9b77a7daf5
humbertoCastro <dedebarbos@hotmail.com>
2021-08-31 13:31:17 -0300

    projeto iniciado e finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,122 @@
-// Desafio 1
-function compareTrue() {
-  // seu código aqui
+//Exercicio 1--- funçao usando operador &&
+function compareTrue(bolean1,bolean2){
+  if(bolean1 && bolean2){
+      return true;
+  }else{
+      return false;
+  }
 }
-
-// Desafio 2
-function calcArea() {
-  // seu código aqui
+//Exericico 2--- Funçao area de triangulo
+function calcArea(base,altura){
+  return (base*altura)/2;
 }
-
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+//Exercicio 3--- Dividir frase
+function splitSentence(frase){
+  let frases = [];
+  let letras = "";
+  let sentença = frase;
+  for(i=0;i<sentença.length+1;i++){
+      if(frase[i]=== " " || i == sentença.length){
+          frases.push(letras);
+          letras = "";
+      }else{
+          letras += frase[i];
+      }
+  }
+  return frases
 }
-
-// Desafio 4
-function concatName() {
-  // seu código aqui
+//Exercicio 4 concatenaçao de strings
+function concatName(frases){
+  return Concatenados = ""+[frases[frases.length-1]+", "+ frases[0]];
 }
-
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
+//Exercicio 5 quantidades de pontos
+function footballPoints(wins,ties){
+  return (wins*3 + ties);
 }
-
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+//Exercicio 6 repetiçao do maior numero
+function highestCount(numeros){
+  let vezesqaparece;
+  let maiornumero = -1000;
+  for(i=0;i< numeros.length+1;i++){
+      if(numeros[i] > maiornumero){
+          maiornumero = numeros[i]
+          vezesqaparece = 1;
+      }
+      else if(numeros[i] == maiornumero){
+          vezesqaparece++;
+      }
+  }
+  return vezesqaparece;
 }
-
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+//Exercicio 7 caça rato
+function catAndMouse(mouse,cat1,cat2){
+  if(Math.abs(mouse - cat1) > Math.abs(mouse - cat2)){
+      return "cat2";
+  }
+  else if(Math.abs(mouse - cat2) > Math.abs(mouse - cat1)){
+      return "cat1";
+  }
+  else{
+      return "os gatos trombam e o rato foge";
+  }
 }
-
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+//Exericico 8 fizzbuzz
+function fizzBuzz(numeros){
+  let respostas = [];
+  for(i=0;i<numeros.length;i++){
+      if(numeros[i]%3 == 0){
+          if(numeros[i]%5 == 0){
+              respostas.push("fizzBuzz");
+          }else{
+              respostas.push("fizz")
+          }
+      }else if(numeros[i]%5 == 0){
+          respostas.push("buzz")
+      }else{
+          respostas.push("bug!");
+      }
+  }
+  return respostas;
 }
-
-// Desafio 9
-function encode() {
-  // seu código aqui
+//Exerciico 9 codifique e descodigique
+function encode(frase){
+  let newfrase = "";
+  for(i=0;i<frase.length;i++){
+      if(frase[i] == "a"){
+          newfrase += "1";
+      }else if(frase[i] == "e"){
+          newfrase += "2";
+      }else if(frase[i] == "i"){
+          newfrase += "3";
+      }else if(frase[i] == "o"){
+          newfrase += "4";
+      }else if(frase[i] == "u"){
+          newfrase += "5";
+      }else{
+          newfrase += frase[i];
+      }
+  }
+  return newfrase;
 }
-function decode() {
-  // seu código aqui
+function decode(frase){
+  console.log("FOI CHAMAD");
+  let newfrase = "";
+  for(i=0;i<frase.length;i++){
+      if(frase[i] == "1"){
+          newfrase += "a";
+      }else if(frase[i] == "2"){
+          newfrase += "e";
+      }else if(frase[i] == "3"){
+          newfrase += "i";
+      }else if(frase[i] == "4"){
+          newfrase += "o";
+      }else if(frase[i] == "5"){
+          newfrase += "u";
+      }else{
+          newfrase += frase[i];
+      }
+  }
+  return newfrase;    
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,23 +1,73 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+
+//Exercicio 10 lista de teconoligas 
+function techList(nomes,name){
+  let resultados = [];
+  const ordenado = nomes;
+  ordenado.sort();    
+  if(nomes.length == 0){
+      return "Vazio!";
+  }    
+  else{
+      for(i=0;i<ordenado.length;i++){
+          console.log();
+          resultados.push({tech: ordenado[i], name : name });
+      }
+      return resultados;
+  }    
 }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+//Exercicio 11 telefone 
+console.log(generatePhoneNumber([0,2,3,4,4,2,7,8,9,9,4]))
+function generatePhoneNumber(numeros){
+  if(numeros.length == 11){
+      let repetiçoes = 0;
+      for(i=0;i<numeros.length;i++){
+          if(numeros[i] <0 || numeros[i] > 9){
+               return "não é possível gerar um número de telefone com esses valores";
+          }
+          else{
+            for(z=0;z<numeros.length;z++){
+              console.log(numeros[i] + "- numero que ta vendo se repete" + numeros[z]+"numero atual que ta comparadnd");
+              if(numeros[i] === numeros[z]){
+                console.log("REPETIU");
+                repetiçoes++;
+                if(repetiçoes ==3){
+                    return "não é possível gerar um número de telefone com esses valores";
+                }            
+              }       
+              console.log("NUMERO DE REPETIÇOES DO NUMERO "+numeros[i] +" --- " + repetiçoes);
+            }
+            repetiçoes = 0;     
+          }
+      }
+      let frase = "(" + numeros[0] + numeros[1]+") "+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+ "-" + numeros[7]+numeros[8]+numeros[9]+numeros[10];
+      return frase;
+  }else{
+      return "Array com tamanho incorreto.";
+  }
 }
-
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
+//Exercicio 12 existencia de triangulo
+function triangleCheck(lineA,lineB,lineC){
+  if( ((lineA + lineB > lineC) &&(Math.abs(lineA-lineB) < lineC)) && ((lineA + lineC > lineB) &&(Math.abs(lineA-lineC) < lineB))  && ((lineC + lineB > lineA) &&(Math.abs(lineC-lineB) < lineA))){
+      return true;
+  }else {
+      return false;
+  }
 }
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
+//Exercicio 13 boas vindas
+function hydrate(txt){
+  //SOLUTION FOUND IN ----> https://stackoverflow.com/questions/10003683/how-can-i-extract-a-number-from-a-string-in-javascript
+  let numb = txt.match(/\d/g);
+  let total = 0;
+  for(i=0;i<numb.length;i++){
+      total += parseInt(numb[i]);
+  }
+  if(total == 1){
+    return total + " copo de água";    
+  }else{
+    return total + " copos de água";   
+  } 
 }
-
 module.exports = {
   generatePhoneNumber,
   techList,
