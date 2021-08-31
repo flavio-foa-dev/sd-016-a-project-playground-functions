dc81707aa766ece8c38d7e955016e7a4b4ff3274
FernandoMos92 <fernandomosp@gmail.com>
2021-08-31 18:05:13 -0300

    completed tas 5

--- src/challenges.js
+++ src/challenges.js
@@ -90,26 +90,131 @@ function concatName(arr) {
 
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+/**
+ * 5 - Crie uma função que calcule a quantidade de pontos no futebol
+Escreva uma função com o nome footballPoints que receba o número de vitórias 
+(esse parâmetro deverá se chamar wins) e o número de empates (esse parâmetro deverá se chamar ties) 
+e retorne a quantidade de pontos que o time marcou em um campeonato.
+
+Para tanto, considere que cada vitória vale 3 pontos e cada empate vale 1 ponto.
+
+O que será verificado:
+
+Retorne 50 pontos quando o time tenha 14 vitórias e 8 empates
+
+Retorne 5 pontos quando o time tenha 1 vitória e 2 empates
+
+Retorne 0 pontos quando o time tenha 0 vitórias e 0 empates
+ * 
+ */
+function footballPoints(wins, ties) {
+
+  let resultWins = 0;
+  let resultTies = 0;
+
+  if(wins === 0 & ties === 0){
+    return 0
+  }
+  if(wins > 0){
+    for(let index = 0; index < wins; index+=1){
+      resultWins += 3;
+    }
+  }
+  if(ties > 0){
+    for(let index = 0; index < ties; index+=1){
+      resultTies += 1;
+    }
+  }
+
+  return resultWins + resultTies; 
 }
 
 // Desafio 6
+/**
+ * 6 - Crie uma função que calcule a repetição do maior número
+Escreva uma função chamada highestCount que, ao receber uma array de números, retorne a quantidade de vezes que o maior deles se repete.
+
+Exemplo: caso o parâmetro de highestCount seja uma array com valores [9, 1, 2, 3, 9, 5, 7], a função deverá retornar 2, que é a quantidade de vezes que o número 9 (maior número do array) se repete.
+
+O que será verificado:
+
+Retorne 2 quando o parâmetro passado na função highestCount seja [9, 1, 2, 3, 9, 5, 7]
+
+Retorne 1 quando o parâmetro passado na função highestCount seja [0, 4, 4, 4, 9, 2, 1]
+
+Retorne 3 quando o parâmetro passado na função highestCount seja [0, 0, 0]
+ */
 function highestCount() {
   // seu código aqui
 }
 
 // Desafio 7
+/**
+ * 7 - Crie uma função de Caça ao Rato
+Imagine que existem dois gatos, os quais chamaremos de cat1 e cat2, e que ambos estão caçando um mesmo rato chamado mouse. Imagine que os animais estão em uma reta, cada um em uma posição representada por um número.
+
+Sabendo disso, crie uma função chamada catAndMouse que, ao receber a posição de mouse, cat1 e cat2, nessa ordem, calcule as distâncias entre o rato e cada um dos gatos, em seguida, retorne qual dos felinos irá alcançar o rato primeiro (aquele que estiver mais perto do rato).
+
+Exemplo: caso o gato cat2 esteja a 2 unidades de distância do rato, e cat1 esteja a 3 unidades, sua função deverá retornar "cat2".
+
+Caso os gatos estejam na mesma distância do rato, a função deverá retornar a string "os gatos trombam e o rato foge".
+
+O que será verificado:
+
+Retorne a string 'cat2' caso a função catAndMouse receba os parâmetros onde gato cat2 esteja a 2 unidades de distância do rato e cat1 esteja a 3 unidades de distância do rato
+
+Retorne a string 'cat1' caso a função catAndMouse receba os parâmetros onde gato cat1 esteja a 6 unidades de distância do rato e cat2 esteja a 12 unidades de distância do rato
+
+Retorne a string 'os gatos trombam e o rato foge' caso a função catAndMouse receba os parâmetros onde os gatos estejam na mesma distância do rato
+ */
 function catAndMouse() {
   // seu código aqui
 }
 
 // Desafio 8
+/**
+ * 8 - Crie uma função FizzBuzz
+Crie uma função chamada fizzBuzz que receba uma array de números e retorne uma array da seguinte forma:
+
+Para cada número do Array que seja divisível apenas por 3, apresente uma string "fizz";
+Para cada número do Array que seja divisível apenas por 5, apresente uma string "buzz";
+Caso o número seja divisível por 3 e 5, retorne a string "fizzBuzz";
+Caso o número não possa ser dividido por 3 nem por 5, retorne a string "bug!";
+Exemplo: caso o parâmetro seja [2, 15, 7, 9, 45], sua função deverá retornar ["bug!", "fizzBuzz", "bug!", "fizz", "fizzBuzz"].
+
+O que será verificado:
+
+Retorne as strings ['bug!', 'fizzBuzz', 'bug!', 'fizz', 'fizzBuzz'] quando é passado os parâmetros [2, 15, 7, 9, 45] para a função fizzBuzz
+
+Retorne as strings ['bug!', 'fizz'] quando é passado os parâmetros [7, 9] para a função fizzBuzz
+
+Retorne as strings ['fizz', 'buzz'] quando é passado os parâmetros [9, 25] para a função fizzBuzz
+ */
 function fizzBuzz() {
   // seu código aqui
 }
 
 // Desafio 9
+/**
+ * 9 - Crie uma função que Codifique e Decodifique
+Crie duas funções: a primeira deverá se chamar encode e, ao receber uma string como parâmetro, deverá trocar todas as vogais minúsculas por números, de acordo com o formato a seguir:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+
+Ou seja, caso o parâmetro de encode seja "hi there!", o retorno deverá ser "h3 th2r2!".
+
+A segunda função deverá se chamar decode e faz o contrário de encode - ou seja, recebe uma string contendo números no lugar de letras minúsculas e retornará uma string com vogais minúsculas no lugar dos números (então, caso o parâmetro de decode seja "h3 th2r2!", o retorno deverá ser "hi there!").
+
+O que será verificado:
+
+Retorne uma string codificada quando a função encode for utilizada
+
+Retorne uma string decodificada quando a função decode for utilizada
+ */
 function encode() {
   // seu código aqui
 }
