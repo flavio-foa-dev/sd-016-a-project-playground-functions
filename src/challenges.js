7b69432f4e555a84a0738ca1db659752169b2efd
PatrickR00t <patrick.s.falquetti@gmail.com>
2021-08-31 17:32:13 +0500

    começando.. aluguns desafios feitos, VQV!

--- src/challenges.js
+++ src/challenges.js
@@ -1,30 +1,37 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true) {
+    return true;
+  }else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base = 10, height = 50) {
+  return (base * altura) / 2;
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
+  let stringExemplo = ['foguete', 'não', 'tem', 'ré'];
+  return stringExemplo;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(param = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']) {
+  let firstLast = param[param.length -1] + param[0];
+  return firstLast;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins = 3, ties = 1) {
+  let points = wins + ties;
+  return points;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(numberGreat = [9, 1, 2, 3, 9, 5, 7]) {
   // seu código aqui
 }
 
