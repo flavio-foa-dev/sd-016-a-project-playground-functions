8fb1231f21c6a9e4a4e3aacd3ed6fc07d3b1e60e
arctavares <cosilk@hotmail.com>
2021-08-31 16:20:28 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,13 +1,13 @@
 // Desafio 10
 function techList(array, name) {
 
-  if(array.length == 0){
+  if (array.length == 0) {
     return 'Vazio!';
   }
 
   array = array.sort();
   let returnArray = [];
-  for(let i = 0; i < array.length; i += 1){
+  for (let i = 0; i < array.length; i += 1) {
     let info = {
       tech: array[i],
       name: name,
@@ -18,8 +18,52 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+
+  let phoneNumber = [];
+  let count = 0;
+
+  for(let i = 0; i < array.length; i += 1){
+    for(let j = 0; j < array.length; i += 1){
+      if(array[i] == array[j]){
+        count += 1;
+      }
+    }
+    if (count >= 3){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    count = 0;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] < 0 || array[i] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    phoneNumber;
+
+    for (let i = 0; i < array.length; i += 1) {
+      if (i == 0) {
+        phoneNumber.push('(');
+        phoneNumber.push(array[i]);
+      } else if (i == 2) {
+        phoneNumber.push(') ');
+        phoneNumber.push(array[i]);
+      } else if (i == 7) {
+        phoneNumber.push('-');
+        phoneNumber.push(array[i]);
+      } else {
+        phoneNumber.push(array[i]);
+      }
+    }
+
+  }
+
+  return phoneNumber.join('');
 }
 
 // Desafio 12
