66537fcef5fce110bed70a0daed8e4f65586d0a1
Igor <iigorfelipe@gmail.com>
2021-08-31 15:49:48 -0300

    finaliza 3 desafios

--- src/challenges.js
+++ src/challenges.js
@@ -1,16 +1,20 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(value1, value2) {
+  if (value1 && value2 === true){
+    return true
+  } else {
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(strings) {
+  return array = strings.split(' ')  
 }
 
 // Desafio 4
