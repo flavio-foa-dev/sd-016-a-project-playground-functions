d653ebdd780769b401d85ed37664c5f943170ff7
Lucas de Oliveira Barboza <lob619@gmail.com>
2021-08-31 15:50:22 -0300

    Requesito 6: check

--- src/challenges.js
+++ src/challenges.js
@@ -23,16 +23,21 @@ function concatName(arrayEx5) { // https://eslint.org/docs/rules/prefer-template
   return `${arrayEx5[arrayEx5.length - 1]}, ${arrayEx5[0]}`; // https://www.guj.com.br/t/gostaria-de-saber-como-posso-pegar-o-ultimo-elemento-de-um-array-em-c/344923
 }
 
-console.log(concatName(['Lucas', 'Iguin', 'Seilá']));
-
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + (ties * 1);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayEx6) {
+  let maior = Math.max.apply(null, arrayEx6); // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+  let contadorMaiorNumero = 0;
+  for (let posicao = 0; posicao < arrayEx6.length; posicao += 1) {
+    if (arrayEx6[posicao] === maior) {
+      contadorMaiorNumero += 1;
+    }
+  }
+  return contadorMaiorNumero;
 }
 
 // Desafio 7
