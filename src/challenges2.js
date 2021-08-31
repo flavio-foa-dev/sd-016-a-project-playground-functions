a24ab1f7dd91e4f1d99f7624f645bf708b0d8aed
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-31 18:59:22 -0300

    finalizado item 9

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,25 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList( listArray, name ) {
+  let newObject = [];
+  let objectList = []; // cria um ojeto vazio
+  let sortedList = []; // cria um array vazio
+  
+  sortedList = listArray.sort(); // Organiza o array
+  
+  if ( listArray.length > 0 ){
+    for( let index = 0; index < sortedList.length; index += 1){
+      //monta um objeto novo
+      objectList[index] =  {
+        tech: sortedList[ index ],
+        name: name
+      }
+    }
+    return objectList;
+  }else {
+    return 'Vazio!';
+  }
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
