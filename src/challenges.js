e4ff6f832ab758fb9f8dbe3a2cf4921f2ea6ad89
FernandoMos92 <fernandomosp@gmail.com>
2021-08-31 17:14:55 -0300

    fourth exercise completed

--- src/challenges.js
+++ src/challenges.js
@@ -62,9 +62,32 @@ function splitSentence(str) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+/**
+ * 4 - Crie uma função que use concatenação de strings
+Escreva uma função com o nome concatName que, ao receber uma array de strings, 
+retorne uma string com o formato 'ÚLTIMO ITEM, PRIMEIRO ITEM', independente do tamanho da array.
+Isso quer dizer que, caso o parâmetro passado para concatName 
+seja a Array ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'], a função deverá retornar Paolillo, Lucas.
+
+O que será verificado:
+Retorne 'Paolillo, Lucas' quando o parâmetro passado na ,
+funcão concatName seja ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']
+
+Retorne 'ré, foguete' quando o parâmetro passado na funcão concatName seja ['foguete', 'não', 'tem', 'ré']
+
+Retorne 'captain, captain' quando o parâmetro passado na funcão concatName seja ['captain', 'my', 'captain']
+ * 
+ */
+
+function concatName(arr) {
+  let lastLetter = arr.slice(-1);
+  let newLetter = arr.slice(0, 1);
+  let result = lastLetter.concat(newLetter);
+   return result.join(', ');
 }
+
+
+
 
 // Desafio 5
 function footballPoints() {
