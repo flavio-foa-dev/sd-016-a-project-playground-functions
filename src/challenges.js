14da05d109c68ab154285cc80817730fb3d222d2
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-08-31 17:48:07 -0300

    Requisito 5

--- src/challenges.js
+++ src/challenges.js
@@ -36,9 +36,14 @@ function concatName(name1) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+//Escreva uma função com o nome footballPoints que receba o número de vitórias (esse parâmetro deverá se chamar wins) e o número de empates 
+//(esse parâmetro deverá se chamar ties) e retorne a quantidade de pontos que o time marcou em um campeonato.
+//Para tanto, considere que cada vitória vale 3 pontos e cada empate vale 1 ponto.
+function footballPoints(wins, ties) {
+  let points = (wins * 3) + (ties * 1);
+  return points;
 }
+console.log(footballPoints(14,8));
 
 // Desafio 6
 function highestCount() {
