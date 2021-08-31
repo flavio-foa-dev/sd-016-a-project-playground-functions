4294fbe452ca980a3ab1ce03779bd68414322be5
joaoOliveira <joaoavelino2008@hotmail.com>
2021-08-31 17:45:35 -0300

    Desafio-6 highestCount

--- src/challenges.js
+++ src/challenges.js
@@ -4,41 +4,53 @@ function compareTrue(param1, param2) {
   if (param1 && param2) {
     return true;
   } else {
-  return false;
+    return false;
   }
 }
 
 // Desafio 2
 function calcArea(base, height) {
   //retorna a area do triangulo
-  return (base*height)/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence(str) {
   // retorna um array com a frase separada em palavras
-  return str.split(" ");
+  return str.split(' ');
 }
 
 // Desafio 4
 function concatName(arr) {
   // retorna o ultimo e primeiro elemento do array concatenados
-  let newArr = [arr[0], arr[arr.length-1]];
-  return newArr[newArr.length-1]+", "+newArr[0];
+  let newArr = [arr[0], arr[arr.length - 1]];
+  return newArr[newArr.length - 1] + ', ' + newArr[0];
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // opera sobre o numero de vitorias e empates e retorna o total somado
-  let vitorias = wins*3;
-  let empates = ties*1;
-  return vitorias+empates;
+  let vitorias = wins * 3;
+  let empates = ties * 1;
+  return vitorias + empates;
 }
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arr) {
+  // verifica qual o maior número
+  let cont = 0;
+  let maior = arr[0];
+  for (let i = 1; i < arr.length; i+=1) {
+    if (arr[i] > maior) {
+      maior = arr[i];
+    }
+  }
+  for(let j = 0; j < arr.length; j+=1) {
+    if(arr[j]===maior){
+      cont +=1;
+    }
+  }
+  return cont;
 }
-
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
