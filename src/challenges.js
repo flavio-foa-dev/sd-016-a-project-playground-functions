e918e1890aa63d37a552156a50ba8152126e5c82
Guilherme <guilhermerhein@gmail.com>
2021-08-31 14:59:17 -0300

    step 2

--- src/challenges.js
+++ src/challenges.js
@@ -6,9 +6,10 @@ if((b1 && b2) === true) {
 return false
  }
 }
+
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) /2
 }
 
 // Desafio 3
