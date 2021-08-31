b589d326a8ca37e46779900898b3d32c345ee53c
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-08-31 17:03:05 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,16 +9,19 @@ function compareTrue(bool1, bool2) {
 
 // Desafio 2
 //Escreva uma função com o nome calcArea que receba um valor de base (chamado base) e outro de altura (chamado height) 
- //de um triângulo e retorne o cálculo da sua área.
+//de um triângulo e retorne o cálculo da sua área.
 //Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2. 
 function calcArea(base, height) {
   let area = (base * height) / 2;
- return area;
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+//Escreva uma função com o nome splitSentence, a qual receberá uma string e retornará uma array de strings separadas por cada espaço na string original.
+//Exemplo: se a função receber a string "go Trybe", o retorno deverá ser ['go', 'Trybe'].
+function splitSentence(stringSplit) {
+  let shareArray = stringSplit.split(" ");
+  return shareArray;
 }
 
 // Desafio 4
