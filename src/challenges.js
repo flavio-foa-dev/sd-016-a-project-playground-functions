b99562df954ea7d77770623698f068da0e192553
Ryogazz <josuex.x.x@hotmail.com>
2021-08-31 16:07:04 -0300

    desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,8 +9,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(palavra) {
+  let corte = [];
+  corte = (palavra.split(' '));
+  return corte
 }
 
 // Desafio 4
