f5894c6f273e05369bef95e61dc175ba6379b1ab
Otavio Cantareli <otavio_cantareli@outlook.com>
2021-08-31 14:57:03 -0300

    Desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -9,41 +9,41 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  return str.split(' ');
 }
 
 // Desafio 4
 function concatName() {
-  // seu código aqui
+  
 }
 
 // Desafio 5
 function footballPoints() {
-  // seu código aqui
+  
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  
 }
 
 // Desafio 7
 function catAndMouse() {
-  // seu código aqui
+  
 }
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  
 }
 
 // Desafio 9
 function encode() {
-  // seu código aqui
+  
 }
 function decode() {
-  // seu código aqui
+  
 }
 
 module.exports = {
