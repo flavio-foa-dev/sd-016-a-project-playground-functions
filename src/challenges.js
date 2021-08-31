41998489a0c42049c2b8670cc4f2783311eae98c
Meiryland Melo <meirylandmelo@gmail.com>
2021-08-31 17:55:50 -0300

    adiciona compareTrue

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,9 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
