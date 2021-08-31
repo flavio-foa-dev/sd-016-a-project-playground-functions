f2c10b328453e583a8e1b349067bdd270fdf63a2
FernandoMos92 <fernandomosp@gmail.com>
2021-08-31 15:29:34 -0300

    completed second exercise

--- src/challenges.js
+++ src/challenges.js
@@ -29,8 +29,21 @@ function compareTrue(a, b) {
 
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+/**
+ * 2 - Crie uma função que calcule a área de um triângulo
+Escreva uma função com o nome calcArea que receba um valor de base (chamado base)
+ e outro de altura (chamado height) de um triângulo e retorne o cálculo da sua área.
+Lembre-se que a área de um triângulo é calculada através da seguinte fórmula: (base * altura) / 2.
+O que será verificado:
+Retorne o valor 250 quando a funcão calcArea é chamada 
+com o parâmetro base com o valor 10 e o parâmetro height com o valor 50
+Retorne o valor 5 quando a funcão calcArea é chamada com o
+parâmetro base com o valor 5 e o parâmetro height com o valor 2 espera-se como resultado 5
+Retorne o valor 25.5 quando a funcão calcArea é chamada 
+com o parâmetro base com o valor 51 e o parâmetro height com o valor 1 espera-se como resultado 25.5
+ */
+function calcArea(base, height){
+  return (base * height)/2
 }
 
 // Desafio 3
