7344929e0ea42d27227021f43e6d7f0d3e988d48
joaogmelo95 <joaogmelo95@gmail.com>
2021-08-31 17:07:00 -0300

    Até o requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,7 +10,12 @@ function techList(arrayTecs, name) {
       };
       newArray.push(learn)
     }
-  
+    
+    /**
+     * Consultas para resolver essa parte
+     * Documentação do método localeCompare: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
+     * Resposta do usuário Blue-eyed Buzzard no fórum: https://www.codegrepper.com/code-examples/javascript/sort+array+of+objects+alphabetically+javascript
+     */
     newArray.sort((a, b) => a.tech.localeCompare(b.tech));
     return newArray;
   } else if (arrayTecs.length === 0) {
@@ -19,9 +24,48 @@ function techList(arrayTecs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumbers) {
+  let ddd = '';
+  let firstHalf = '';
+  let secondHalf = '';
+  let repeticoes = 0;
+
+  if (arrayNumbers.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let numbers of arrayNumbers) {
+    if (numbers < 0 || numbers > 9) {
+      console.log('<0 ou >9')
+      return 'não é possível gerar um número de telefone com esses valores';
+    } else {
+      for (let numbers2 of arrayNumbers) {
+        if (numbers === numbers2) {
+          repeticoes += 1;
+          if (repeticoes >= 3) {
+            console.log('x3')
+            return 'não é possível gerar um número de telefone com esses valores';
+          }
+        }
+      }
+      repeticoes = 0;
+    }
+  }
+
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (index < 2) {
+      ddd += arrayNumbers[index];
+    } else if (index >= 2 && index < 7) {
+      firstHalf += arrayNumbers[index];
+    } else if (index >= 7) {
+      secondHalf += arrayNumbers[index];
+    }
+  }
+
+  return '('+ ddd + ') ' + firstHalf + '-' + secondHalf; 
 }
+
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 // Desafio 12
 function triangleCheck() {
