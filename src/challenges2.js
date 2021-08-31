e1ee02e31c6b948d75a334a63e83043665de4fb3
Tiago Abranges <tiagoabranges@gmail.com>
2021-08-31 18:16:49 -0300

    requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,9 +23,18 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if (a + b < c || Math.abs(a - b) > c) {
+    return false;
+  } else if (a + c < b || Math.abs(a - c) > b) {
+    return false;
+  } else if (c + b < a || Math.abs(c - b) > a) {
+    return false;
+  } else {
+    return true;
+  }
 }
+
 
 // Desafio 13
 function hydrate() {
