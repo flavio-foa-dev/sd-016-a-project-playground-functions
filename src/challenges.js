87cbd60bdd818d586bbbcdf5ded52421ec5e8a9a
CamilaRanniele <rannielecamsil@gmail.com>
2021-08-31 18:05:34 -0300

    Modificando desafio 6

--- src/challenges.js
+++ src/challenges.js
@@ -60,7 +60,7 @@ function catAndMouse(mouse, cat1, cat2) {
     }else if (distanceCat2 < 0){
       distanceCat2 = distanceCat2 * (-1);
     }
-    
+
     if ( distanceCat2 === distanceCat1){
       return 'os gatos trombam e o rato foge'
     }else if (cat1 < cat2){
@@ -89,8 +89,24 @@ function fizzBuzz(arr) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let newString ;
+  for (let i = 0; i < string.length; i += 1){
+    if (string[i] === 'a'){
+      newString[i] = '1';
+    }else if (string[i] === 'e'){
+      newString[i] = '2';
+    }else if (string[i] === 'i'){
+      newString[i] = '3';
+    }else if (string[i] === 'o'){
+      newString[i] = '4';
+    }else if (string[i] === 'e'){
+      newString[i] = '5';
+    }else{
+      newString[i] = string[i];
+    }
+  }
+  return newString
 }
 function decode() {
   // seu código aqui
