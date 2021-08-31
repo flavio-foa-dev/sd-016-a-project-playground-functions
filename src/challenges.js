cdee50873fa3c5cfa389a346e6c514119b02f776
emmanoel <emmanoelporto@gmail.com>
2021-08-31 17:24:33 -0300

    Requisito 3 Concluido

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(word) {
+  let resultadoSplit = word.split(' ');
+  return resultadoSplit;
 }
 
 // Desafio 4
