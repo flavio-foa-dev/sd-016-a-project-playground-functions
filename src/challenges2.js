4970a862e16bfbeb02a2b73f35ccce9029bc8a21
sergio3213 <sergio.n.m.jr@gmail.com>
2021-09-01 12:39:21 -0300

    ate o exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,24 @@
 // Desafio 10
-function techList() {
-
+function techList(array,name) {
   // seu código aqui
+  if(array.length==0){
+    return "Vazio!"
+  }
+  else{
+      array_ordenada=array.sort()
+      for(cont=0;cont<array_ordenada.length;cont+=1){
+        array_ordenada[cont] = {
+          tech:array_ordenada[cont],
+          name:name,
+        }
+      }
+      
+      
+        return array_ordenada
+      }
 }
+
+console.log(techList([],"sergio"))
 
 // Desafio 11
 function generatePhoneNumber() {
