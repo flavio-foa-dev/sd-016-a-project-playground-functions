b1f24f0e30fba95dfca53506d06f5b7163347380
joaoOliveira <joaoavelino2008@hotmail.com>
2021-09-01 11:45:29 -0300

    Desafio-9.2 decode

--- src/challenges.js
+++ src/challenges.js
@@ -65,49 +65,70 @@ function fizzBuzz(arr) {
       newArr.push('fizzBuzz');
       //se divisivel por 3 retorna "fizz"
     } else if (arr[index] % 3 == 0) {
-       newArr.push('fizz');
-       //se divisivel por 5 retorna "buzz"
+      newArr.push('fizz');
+      //se divisivel por 5 retorna "buzz"
     } else if (arr[index] % 5 == 0) {
-       newArr.push('buzz');
-       //se nao divisivel por 5 ou 3 retorna "bug!"
+      newArr.push('buzz');
+      //se nao divisivel por 5 ou 3 retorna "bug!"
     } else if (arr[index] % 5 !== 0 || arr[index] % 3 !== 0) {
-       newArr.push('bug!');
+      newArr.push('bug!');
     }
   }
   //retorna o novoArray
   return newArr;
 }
 
-
 // Desafio 9
 function encode(str) {
-  // seu código aqui
-  let result = str.split(""); 
+  //separa a string por caractere e declara array
+  let result = str.split('');
   let final = [];
-  for(let i in result){
-    if(result[i]==="a"){
+  //itera sobre o array e converte as strings
+  for (let i in result) {
+    if (result[i] === 'a') {
       result[i] = 1;
     }
-    if(result[i]==="e"){
+    if (result[i] === 'e') {
       result[i] = 2;
     }
-    if(result[i]==="i"){
+    if (result[i] === 'i') {
       result[i] = 3;
     }
-    if(result[i]==="o"){
+    if (result[i] === 'o') {
       result[i] = 4;
     }
-    if(result[i]==="u"){
+    if (result[i] === 'u') {
       result[i] = 5;
     }
     final = result.join('');
   }
   return final;
 }
-encode("hello");
 
-function decode() {
-  // seu código aqui
+function decode(str) {
+  //separa a string por caractere e declara array
+  let result = str.split('');
+  let final = [];
+  //itera sobre o array e converte as strings
+  for (let i in result) {
+    if (result[i] === '1') {
+      result[i] = 'a';
+    }
+    if (result[i] === '2') {
+      result[i] = 'e';
+    }
+    if (result[i] === '3') {
+      result[i] = 'i';
+    }
+    if (result[i] === '4') {
+      result[i] = 'o';
+    }
+    if (result[i] === '5') {
+      result[i] = 'u';
+    }
+    final = result.join('');
+  }
+  return final;
 }
 
 module.exports = {
