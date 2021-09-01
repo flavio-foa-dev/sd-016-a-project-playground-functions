ba65a8d0fde73ee9df8cdcd3f6d3688c8a562ed3
Leandro Bonfim <leandrobonfim15@homtial.com>
2021-08-31 23:45:05 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-else-return */
+/* eslint-disable sonarjs/no-unused-collection */
+/* eslint-disable complexity */
+/* eslint-disable sonarjs/cognitive-complexity */
 // Desafio 10
 function techList(arrayTech, name) {
   let arrayObject = [];
@@ -19,8 +23,33 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+// eslint-disable-next-line complexity
+// eslint-disable-next-line max-lines-per-function
+function generatePhoneNumber(arrayPhone) {
+  // (12) 34567-8901
+  let result = '';
+  let numerCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  if (arrayPhone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arrayPhone.length; index += 1) {
+    if (arrayPhone[index] > 9 || arrayPhone[index] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    numerCount[arrayPhone[index]] += 1;
+    if (numerCount[arrayPhone[index]] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    } else if (index === 0) {
+      result += '(';
+    } else if (index === 2) {
+      result += ') ';
+    } else if (index === 7) {
+      result += '-';
+    }
+    result += arrayPhone[index];
+  }
+  return result;
 }
 
 // Desafio 12
