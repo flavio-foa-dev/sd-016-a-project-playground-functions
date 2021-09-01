ac6c3ed68a0f85785cac72823cec7e5d3b810e38
joaoOliveira <joaoavelino2008@hotmail.com>
2021-09-01 12:34:23 -0300

    Desafio-10 techList

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arr, name) {
+  //declara array auxiliar
+  let newArray = [];
+  //retorna vazio caso o array recebido seja == null
+  if(arr.length === 0){
+    return 'Vazio!'; 
+  }
+  //itera sobre o array recebido como argumento e insere os dados recebidos no array auxiliar
+  for(let index in arr) {
+    newArray.push({tech: arr[index],name: name},);
+  }
+  //retorna o array auxiliar ordenado em ordem alfabetica
+  return newArray.sort((a,b)=>{
+    if (a.tech < b.tech)
+      return -1;
+    else
+      return 1;
+  });
 }
+
 
 // Desafio 11
 function generatePhoneNumber() {
