65ff2d4277a9e9eefd56624d4e38a565a1366026
lucasdoloh0v <lucasdolohov@gmail.com>
2021-09-01 07:45:16 -0300

    requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -68,21 +68,33 @@ function catAndMouse(mouse, cat1, cat2) {
     distance1 = cat1 - mouse;
   }
   if (cat2 > mouse) {
-    distance2 = cat2 - mouse
+    distance2 = cat2 - mouse;
   }
   if (distance1 < distance2) {
-    return 'cat1'
+    return 'cat1';
   } else if (distance1 > distance2) {
-    return 'cat2'
+    return 'cat2';
   } else {
-    return 'os gatos trombam e o rato foge'
+    return 'os gatos trombam e o rato foge';
   }
 }
-console.log(catAndMouse(0, 3, 2))
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
   // seu código aqui
+  let resultArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 5 === 0 && array[i] % 3 === 0) {
+      resultArray.push('fizzBuzz');
+    } else if (array[i] % 5 !== 0 && array[i] % 3 === 0) {
+      resultArray.push('fizz');
+    } else if (array[i] % 5 === 0 && array[i] % 3 !== 0) {
+      resultArray.push('buzz');
+    } else {
+      resultArray.push('bug!');
+    }
+  }
+  return resultArray;
 }
 
 // Desafio 9
