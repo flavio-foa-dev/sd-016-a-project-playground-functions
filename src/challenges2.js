abec523593f877682ef6fe513578863deeab6488
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-08-31 23:47:02 -0300

    adicionado desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -12,13 +12,45 @@ function techList(techN, pName) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayList) {
+  if (arrayList.length > 11 || arrayList.length < 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (Math.min.apply(null, arrayList) < 0 || Math.max.apply(null, arrayList) > 9) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  // for (let i = 0; i < arrayList.length; i += 1) {
+  //   for (let j = 0; j < arrayList.length; j += 1) {
+  //     if (arrayList[j] === arrayList[i]) {
+  //       cont += 1;
+  //     } else {
+  //       cont += 0;
+  //     }
+  //   } return cont;
+  // }
+  return `(${arrayList[0]}${arrayList[1]}) ${arrayList[2]}${arrayList[3]}${arrayList[4]}${arrayList[5]}${arrayList[6]}-${arrayList[7]}${arrayList[8]}${arrayList[9]}${arrayList[10]}`;
 }
 
+// arrayList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
+// console.log(generatePhoneNumber(arrayList))
+
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let checkLineA = false;
+  let checkLineB = false;
+  let checkLineC = false;
+  if (lineA < (lineB + lineC) && lineA > Math.abs(lineB - lineC) && lineA > Math.abs(lineC - lineB)) {
+    checkLineA = true;
+  }
+  if (lineB < (lineC + lineA) && lineB > Math.abs(lineC - lineA) && lineB > Math.abs(lineA - lineC)) {
+    checkLineB = true;
+  }
+  if (lineC < (lineB + lineA) && lineC > Math.abs(lineB - lineA) && lineC > Math.abs(lineA - lineB)) {
+    checkLineC = true;
+  }
+  if (checkLineA === true && checkLineB === true && checkLineC === true) {
+    return true;
+  } return false;
 }
 
 // Desafio 13
