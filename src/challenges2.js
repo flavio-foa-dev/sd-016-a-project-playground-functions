f707100130874a68ddad52cc750f9f907d0c4a76
Ricielle Cheloni <riciellecheloni@gmail.com>
2021-08-31 21:34:53 -0300

    Requisito 10 - correção

--- src/challenges2.js
+++ src/challenges2.js
@@ -60,10 +60,11 @@ Bônus
 
 */
 function techList(nameTech, name) {  
-    let arrTechs = [];    
+    let arrTechs = [];  
+    nameTech = nameTech.sort(); 
      if (nameTech.length === 0) {
         return 'Vazio!';
-      } else
+      } 
       for (let index = 0; index < nameTech.length; index += 1) {
       arrTechs.push({
         tech: nameTech[index],        
@@ -74,11 +75,44 @@ function techList(nameTech, name) {
 }
    
 
-let resposta = techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Ricielle");
-console.log(resposta);
+//let resposta = techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Ricielle");
+//console.log(resposta);
 
 
 // Desafio 11
+
+/*
+11 - Crie uma função de Número de Telefone
+Crie uma função chamada generatePhoneNumber que receba uma array com 11 números e retorne um 
+número de telefone, respeitando parênteses, traços e espaços.
+
+Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], generatePhoneNumber 
+deverá retornar (12) 34567-8901.
+
+Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho 
+incorreto.".
+
+Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, 
+generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses 
+valores".
+
+O que será verificado:
+
+Retorne a string "Array com tamanho incorreto." caso o array tenha o tamanho diferente de 11
+
+Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum dos números 
+do array seja menor que 0
+
+Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do
+array seja maior que 9
+
+Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum número do 
+array se repetir 3 vezes ou mais
+
+Retorne um número de telefone, respeitando parênteses, traços e espaços caso os números do array 
+estejam de acordo com as especificações
+
+*/
 function generatePhoneNumber() {
   // seu código aqui
 }
