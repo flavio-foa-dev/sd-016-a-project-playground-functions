ba8441df7317da0c0f04740871eb0bd7385704c3
Esau Matias <esaufreirematias@gmail.com>
2021-09-01 18:08:18 -0300

    Refatoraçao do exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,11 +11,7 @@ function techList(tech, name) {
 }
 // Desafio 11
 function generatePhoneNumber(array) {
-  if (array.length === 11) {
-    return '(' + array[0] + array[1] + ') ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
-  } if (array.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
+  
 }
 
 // Desafio 12
