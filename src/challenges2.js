09212d3147cf038c49ca57c2501acdcb11680a39
Ryogazz <josuex.x.x@hotmail.com>
2021-09-01 13:38:33 -0300

    desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,36 @@ function techList(lista, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numero) {
+  let cont = 0;
+  let cont2 = 0;
+  let x;
+  if (numero.length > 11 || numero.length < 11) {
+    return ("Array com tamanho incorreto.")
+  }
+  for (let index in numero) {
+    if (numero[index] < 0 || numero[index] > 9) {
+      return ("não é possível gerar um número de telefone com esses valores");
+    }
+    for (let index2 in numero) {
+
+      if (numero[index] === numero[index2]) {
+        cont++;
+      }
+    }
+
+    if (cont > cont2) {
+      cont2 = cont
+    }
+    cont = 0;
+  }
+  if (cont2 >= 3) {
+    return ("não é possível gerar um número de telefone com esses valores");
+  }
+
+  let telefone = ('(' + (numero[0] + '' + numero[1]) + ') ' + numero[2] + '' + numero[3] + '' + numero[4] + '' + numero[5] + '' + numero[6] + "-" + numero[7] + '' + numero[8] + '' + numero[9] + '' + numero[10]);
+
+  return (telefone);
 }
 
 // Desafio 12
