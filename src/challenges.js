1c7026615aae3c1e7010dbda23bb19f53fcd133e
Paulini <guiguigui45@hotmail.com>
2021-08-31 22:50:39 -0300

    Requisito 6.

--- src/challenges.js
+++ src/challenges.js
@@ -1,49 +1,65 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if(value1 === true && value2 === true){
+  if (value1 === true && value2 === true) {
     return true;
-  }else{
+  } else {
     return false;
   }
 }
 
 // Desafio 2
-function calcArea(base,height) {
- let area = 0; 
- area = (base*height)/2;
- console.log(area);
+function calcArea(base, height) {
+  let area = 0;
+  area = (base * height) / 2;
+  console.log(area);
   return area;
 }
 
 // Desafio 3
 function splitSentence(string) {
-let array =string.split(" ");
-return array ; 
+  let array = string.split(" ");
+  return array;
 
 }
 
 // Desafio 4
 function concatName(names) {
- let firstName = names[0];
- let lastName = names[names.length -1];
- let newNames = lastName + ", " + firstName;
- return newNames ;
- 
+  let firstName = names[0];
+  let lastName = names[names.length - 1];
+  let newNames = lastName + ", " + firstName;
+  return newNames;
+
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  
-  let winsPoints = wins * 3 ;
+
+  let winsPoints = wins * 3;
   let tiesPoints = ties * 1;
   let endGame = winsPoints + tiesPoints;
   return endGame;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+
+function highestCount(numbers) {
+  // Declarando variaveis fora do for.
+  let array = numbers[0];
+  let contador = 0;
+  //Abrindo laço de repetição
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > array) {
+     array = numbers[index];
+    } 
+  } 
+  for(let i=0; i < numbers.length;i +=1){
+    if(numbers[i] === array){
+      contador += 1;
+    }
+  } return contador;
+  
+} 
+
 
 // Desafio 7
 function catAndMouse() {
