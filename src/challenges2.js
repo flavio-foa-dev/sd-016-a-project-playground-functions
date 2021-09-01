bb75b8ce0d84a669e045e807c1512e3551414d94
Emerson <eemr3@yahoo.com.br>
2021-08-31 22:54:39 -0300

    Decumo requisito - Lista de Tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function criaNewObject(arrayTech, name) {
+  const newCollectionArrayObject = [];
+  for (let index = 0; index < arrayTech.length; index += 1) {
+    const objectCollection = { tech: '', name: '' };
+    objectCollection.tech = arrayTech[index];
+    objectCollection.name = name;
+
+    newCollectionArrayObject.push(objectCollection);
+  }
+  return newCollectionArrayObject;
+}
+
+function techList(arrayTech, name) {
+  arrayTech = arrayTech.sort();
+  const newCollectionArrayObject = criaNewObject(arrayTech, name);
+  if (arrayTech.length === 0) {
+    return 'Vazio!';
+  }
+  return newCollectionArrayObject;
 }
 
 // Desafio 11
