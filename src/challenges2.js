e6d73403373f95795554a280f08c77b57bc34544
alt-art <pedromendescraft@gmail.com>
2021-08-31 23:06:44 -0300

    Challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,56 @@
 // Desafio 10
 function techList(array, name) {
-  if (array.length <= 0)
-    return "Vazio!";
+  if (array.length <= 0) {
+    return 'Vazio!';
+  }
   let arraySort = array.sort();
   let result = [];
-  for (elem of arraySort) {
+  for (let elem of arraySort) {
     result.push({
       tech: elem,
-      name: name,
-    })
+      name,
+    });
   }
   return result;
 }
 
+function repeatCount(array) {
+  let unique = Array.from(new Set(array));
+  let lastResult = 0;
+  for (let elem of unique) {
+    let result = 0;
+    for (let num of array) {
+      if (elem === num) {
+        result += 1;
+      }
+    }
+    if (result > lastResult) {
+      lastResult = result;
+    }
+  }
+  return lastResult;
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  const Numb = array;
+  if (Numb.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let result = '(';
+  for (let i = 0; i < Numb.length; i += 1) {
+    if (Numb[i] > 9 || Numb[i] < 0 || repeatCount(Numb) >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    result += Numb[i];
+    if (i === 1) {
+      result += ') ';
+    }
+    if (i === 6) {
+      result += '-';
+    }
+  }
+  return result;
 }
 
 // Desafio 12
