142bddfa87f52a1871975c163bf1ec29d4f910e7
CamilaRanniele <rannielecamsil@gmail.com>
2021-09-01 14:06:19 -0300

    Modificando erros do eslint

--- src/challenges.js
+++ src/challenges.js
@@ -6,42 +6,40 @@ function compareTrue(valor1, valor2) {
 // Desafio 2
 function calcArea(base, height) {
   let calculo = (base * height) / 2;
-  return calculo
+  return calculo;
 }
 
 // Desafio 3
 function splitSentence(stringSplit) {
   let arr = stringSplit.split(' ');
-  return arr
+  return arr;
 }
 
 // Desafio 4
 function concatName(array) {
-  let string = array[array.length-1] + ", "+ array[0]
-  return string
+  let string = array[array.length -1] + ', ' + array[0];
+  return string;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let calc = (wins * 3) + (ties *1)
-  return calc
+  let calc = (wins * 3) + (ties * 1);
+  return calc;
 }
 
 // Desafio 6
 function highestCount(arr) {
-    let highestNumber = arr[0];
-    let sum = 1;
-    for (let index = 1; index < arr.length; index += 1){
-        if (arr[index] > highestNumber){
-            
-            highestNumber = arr[index];
-            sum = 1;
-
-        }else if ( arr[index] === highestNumber){
-            sum += 1;
-        }
+  let highestNumber = arr[0];
+  let sum = 1;
+  for (let index = 1; index < arr.length; index += 1) {
+    if (arr[index] > highestNumber) {
+      highestNumber = arr[index];
+      sum = 1;
+    } else if (arr[index] === highestNumber) {
+      sum += 1;
     }
-    return sum
+  }
+  return sum;
 }
 // [ 2, 3 , 5 ,2 4]
 
@@ -53,35 +51,34 @@ function catAndMouse(mouse, cat1, cat2) {
     if ( distanceCat1 < 0){
       distanceCat1 = distanceCat1 * (-1);
     }
-    if (distanceCat2 < 0){
+    if (distanceCat2 < 0) {
       distanceCat2 = distanceCat2 * (-1);
     }
     console.log(distanceCat1 , distanceCat2);
     if ( distanceCat2 === distanceCat1){
       return 'os gatos trombam e o rato foge'
-    }else if (distanceCat1 < distanceCat2){
-        return 'cat1'
-    }else if (distanceCat2 < distanceCat1){
-        return 'cat2' 
+    }else if (distanceCat1 < distanceCat2) {
+      return 'cat1'
+    }else if (distanceCat2 < distanceCat1) {
+      return 'cat2' 
     }
 }
 
-
 // Desafio 8
 function fizzBuzz(arr) {
-      let array = [];
-    for (let index = 0; index < arr.length; index += 1){
-        if (arr[index] % 3 === 0 && arr[index] % 5 === 0){
-            array.push('fizzBuzz');
-        }else if (arr[index] % 5 === 0){
-            array.push('buzz');
-        }else if (arr[index] % 3 === 0){
-            array.push('fizz');
-        }else{
-            array.push('bug!')
-        }
+  let array = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] % 3 === 0 && arr[index] % 5 === 0) {
+      array.push('fizzBuzz');
+    } else if (arr[index] % 5 === 0) {
+      array.push('buzz');
+    } else if (arr[index] % 3 === 0) {
+      array.push('fizz');
+    } else{
+      array.push('bug!');
     }
-    return array
+  }
+  return array;
 }
 
 // Desafio 9
