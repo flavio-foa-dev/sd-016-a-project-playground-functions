ab222a4cbd2a648f185e154233ff46e6f44ee0c5
Yngwie Kurebayashi <yngwie.rs@gmail.com>
2021-09-01 17:41:08 -0300

    Funcoes 7 a 9

--- src/challenges.js
+++ src/challenges.js
@@ -45,23 +45,86 @@ function highestCount(value) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if(Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
+    return "cat2";
+  } else if(Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+    return "cat1";
+  }
+  if(Math.abs(cat1 - mouse) === Math.abs(cat2 - mouse)){
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(values) {
+  //Pegando o tamanho do array
+  arrayLenght = values.length;
+
+    //correndo todos os numeros do array
+    for(i = 0; i < arrayLenght; i++){
+
+      //setando que inicialmente, nada pode se dividiro por 3 nem por 5
+      canDivideBy3 = false;
+      canDivideBy5 = false;
+
+      //checando para 3
+      if(values[i] % 3 === 0){
+        canDivideBy3 = true;
+
+        //checando para 5
+        if(values[i] % 5 === 0){
+
+          //se valer para os dois printar fizzbuzz
+          if(canDivideBy3 === true){
+            values[i] = "fizzBuzz";
+          }
+        } else{ //caso seja so para o 3 imprimir fizz
+          values[i] = "fizz";
+        }    
+      } else if(values[i] % 5 === 0){ // caso seja so para o 5 imprimir buzz
+        canDivideBy5 = true;
+        values[i] = "buzz";
+      }
+
+      //caso nao seja para nenhum dos dois
+      if(canDivideBy3 === false && canDivideBy5 === false){
+        values[i] = "bug!";
+      }
+  }
+  return values;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let lettersNumbers = ["a","e","i","o","u"];
+  result = phrase.split("");
+  for(i = 0; i < phrase.length; i++){
+    for(x = 0; x < lettersNumbers.length; x++){
+      if(result[i] === lettersNumbers[x]){
+        result[i] = x+1;
+      }
+    }
+  }
+
+  return result.join("");
 }
-function decode() {
-  // seu código aqui
+function decode(phrase) {
+  let lettersNumbers = ["1","2","3","4","5"];
+  let numbersLetters = ["a","e","i","o","u"];
+  result = phrase.split("");
+  for(i = 0; i < phrase.length; i++){
+    for(x = 0; x < lettersNumbers.length; x++){
+      if(result[i] === lettersNumbers[x]){
+        result[i] = numbersLetters[x];
+      }
+    }
+  }
+
+  return result.join("");
 }
 
+console.log(decode("h3 th2r2!"));
 module.exports = {
   calcArea,
   catAndMouse,
