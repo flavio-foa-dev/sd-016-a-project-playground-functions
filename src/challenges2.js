d116c7b6afa46d878322d74ba40e7d9edfdcabba
joaoOliveira <joaoavelino2008@hotmail.com>
2021-09-01 18:54:07 -0300

    Desafio-12 checkRepeat

--- src/challenges2.js
+++ src/challenges2.js
@@ -35,29 +35,48 @@ function generatePhoneNumber(arr) {
   return "(" + newArray[0]+newArray[1]+")"+ " " + newArray.splice(2,6).join('')+ "-" + finalNumbers;
 
 }
-generatePhoneNumber([4, 2, 1, 3, 4, 5, 6, 7, 8, 9, 1]);
-
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
 
 function checkRepeat(array) {
-    let repeat = Array.from(new Set(array));
-    let occour = 0;
-    for (let element of repeat) {
-      let result = 0;
-      for (let n of array) {
-        if (element === n) {
-          result += 1;
-        }
-      }
-      if (result > occour) {
-        occour = result;
+  let repeat = Array.from(new Set(array));
+  let occour = 0;
+  for (let element of repeat) {
+    let result = 0;
+    for (let n of array) {
+      if (element === n) {
+        result += 1;
       }
     }
-    return occour;
+    if (result > occour) {
+      occour = result;
+    }
   }
+  return occour;
+}
+
+// Desafio 12
+function triangleCheck(lineA, lineB, lineC) {
+  //retorna false caso a soma dos lados for menor que um dos lados 
+  if (lineA>sumLados(lineB,lineC) || lineB>sumLados(lineA,lineC) || lineC>sumLados(lineA,lineB)){
+    return false;
+  } 
+  //retorna false caso a diferenca absoluta dos lados for maior que um dos lados
+  if (lineA<difLados(lineB,lineC) || lineB<difLados(lineA,lineC) || lineC<difLados(lineA,lineB)){
+    return false;
+  } else {
+    //retorna true caso contrario
+    return true;
+  }
+}
+
+//Desafio 12 - funcao auxiliar de soma 
+function sumLados(a,b){
+  return a+b;
+}
+
+//Desafio 12 - funcao auxiliar de diferenca
+function difLados(a,b){
+  return Math.abs(a-b);
+}
 
 
 // Desafio 13
