4347747f5d80d2dcb50bd32ebe5b0cfbb7bac166
Ricardo Carvalho <ricardomc.dev@gmail.com>
2021-09-01 09:11:01 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -52,28 +52,38 @@ function highestCount(arr) {
 function catAndMouse(mouse, cat1, cat2) {
   let mouseCat1Pos = mouse - cat1;
   let mouseCat2Pos = mouse - cat2;
-  
   if (mouseCat1Pos < 0) {
-    mouseCat1Pos = mouseCat1Pos * -1;
+    mouseCat1Pos *= -1;
   }
   if (mouseCat2Pos < 0) {
-    mouseCat2Pos = mouseCat2Pos * -1;
-  } 
+    mouseCat2Pos *= -1;
+  }
   if (mouseCat1Pos < mouseCat2Pos) {
-    return 'cat1'
+    return 'cat1';
   } else if ( mouseCat1Pos > mouseCat2Pos) {
-    return 'cat2'
+    return 'cat2';
   } else {
-    return 'os gatos trombam e o rato foge'
+    return 'os gatos trombam e o rato foge';
   }
 }
-console.log(catAndMouse(1, 0, 2))
-
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz(arrNumber) {
-  let arrFB
+  for (let i = 0; i < arrNumber.length; i += 1) {
+    if (arrNumber[i] % 3 === 0 && arrNumber[i] % 5 ===0) {
+      arrNumber.fill('fizzBuzz', i, i+1);
+    } else if (arrNumber[i] % 3 === 0) {
+      arrNumber.fill('fizz', i, i+1);
+    } else if (arrNumber[i] % 5 === 0){
+      arrNumber.fill('buzz', i, i+1);
+    } else {
+      arrNumber.fill('bug!', i, i+1);
+    }
+  }
+  return arrNumber;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
