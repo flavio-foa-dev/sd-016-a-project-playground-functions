b46884c245e579d6165a3579207eabd8f3438086
Igor <iigorfelipe@gmail.com>
2021-08-31 22:54:15 -0300

    finaliza desafio do fizzbuzz

--- src/challenges.js
+++ src/challenges.js
@@ -51,8 +51,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let array2 = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if(array[i] % 3 != 0 && array[i] % 5 != 0){
+      array2.push('bug!');
+    } else if (array[i] % 3 === 0 && array[i] % 5 === 0){
+      array2.push('fizzBuzz');
+    } else if (array[i] % 3 === 0) {
+      array2.push('fizz');
+    } else if (array[i] % 5 === 0) {
+      array2.push('buzz');
+    }
+  }
+  return array2;
 }
 
 // Desafio 9
