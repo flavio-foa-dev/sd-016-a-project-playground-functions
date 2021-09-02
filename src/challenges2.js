db8ef58630f416504561ce03745e7df882cefc76
Gabriele Castro <gabrielerayane2001@gmail.com>
2021-09-02 12:36:06 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,55 @@
 // Desafio 10
 function techList(tecnologias, name) {
-  // seu código aqui
+ /*  let lista = {
+    tech: '',
+    name: ''
+  }; 
+
+  for (let i = 0; i < tecnologias.length; i += 1) {
+    let lista.tech = tecnologias[i];
+
+  } */
 }
+ 
+/* console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas')); */
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let telefone = [];
+  if (array.length != 11) {
+    return "Array com tamanho incorreto.";
+  }
+  for (let i = 0; i <= 1; i += 1){
+    telefone = ('(' + array[0] + array[1] + ')');
+  }
+    telefone += " ";
+
+  for (let j = 2; j <= 6; j += 1) {
+    telefone += array[j]; 
+  }
+
+    telefone += "-";
+
+  for (let l = 7; l <= 10; l += 1) {
+    telefone += array[l];
+  }
+
+  let repete = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] < 0 || array[i] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+        for (let key in array){
+          if (array[i] === array[key]){
+            repete += 1;
+          }
+        }
+          if (repete >= 3) {
+            return "não é possível gerar um número de telefone com esses valores";
+        }
+        repete = 0;
+    }
+    return telefone;
 }
 
 // Desafio 12
