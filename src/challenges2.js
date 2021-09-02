400642965c0ab0c0f215f2e14cdb89682a044869
Fernanda9421 <fer.candrade21@gmail.com>
2021-09-02 10:49:06 -0300

    Resoluçao do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,45 @@ function techList(tech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let phoneNumber;
+
+  if (numbers.length !== 11) {
+    phoneNumber = 'Array com tamanho incorreto.';
+  } else if (numbers.length === 11) {
+    for (let index = 0; index < numbers.length; index += 1) {
+      if (numbers[index] < 0 || numbers[index] > 9) {
+        phoneNumber = 'não é possível gerar um número de telefone com esses valores';
+        break;
+      } else {
+        let ddd = numbers.slice(0, 2);
+        let firstPart = numbers.slice(2, 7);
+        let secondPart = numbers.slice(7, 11);
+
+        phoneNumber = `(${ddd.join('')}) ${firstPart.join('')}-${secondPart.join('')}`;
+      }
+    }
+  }
+
+  if (numbers.length === 11) {
+    let repeatCounter = 0;
+
+    for (let indexCurrent = 0; indexCurrent < numbers.length; indexCurrent += 1) {
+      let currentNumber = numbers[indexCurrent];
+
+      for (let index = 0; index < numbers.length; index += 1) {
+        if (currentNumber === numbers[index]) {
+          repeatCounter += 1;
+        }
+        if (repeatCounter >= 3) {
+          phoneNumber = 'não é possível gerar um número de telefone com esses valores';
+          break;
+        }
+      }
+      repeatCounter = 0;
+    }
+  }
+  return phoneNumber;
 }
 
 // Desafio 12
