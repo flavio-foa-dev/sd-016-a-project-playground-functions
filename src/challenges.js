b927ac787084382fb4953068e6b406e16cc4a0a0
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-02 20:17:42 -0300

    Resolução do desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -49,9 +49,26 @@ function highestCount(arrayNumbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceMouseCat1 = cat1 - mouse;
+  let distanceMouseCat2 = cat2 - mouse;
+  let msg;
+  if (distanceMouseCat1 < 0) {
+    distanceMouseCat1 *= (-1);
+  } else if (distanceMouseCat2 < 0) {
+    distanceMouseCat2 *= (-1);
+  }
+  if (distanceMouseCat1 < distanceMouseCat2) {
+    msg = 'cat1';
+  } else if (distanceMouseCat2 < distanceMouseCat1) {
+    msg = 'cat2';
+  } else {
+    msg = 'os gatos trombam e o rato foge';
+  }
+  return msg;
 }
+let result = catAndMouse(1, 4, 3);
+console.log(result);
 
 // Desafio 8
 function fizzBuzz() {
