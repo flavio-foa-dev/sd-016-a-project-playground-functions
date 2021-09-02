6ed37ad5db5dfbcb817da10924fa839915b72893
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-09-02 00:05:37 -0300

    Finalizado Item 11 - Numero Telefonico

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,9 +22,50 @@ function techList( listArray, name ) {
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber( number ) { //(51)12345-6789
+  let numberResult = [];
+  let myVar = '';
+  let errorCode1 = "Array com tamanho incorreto.";
+  let errorCode2 = "não é possível gerar um número de telefone com esses valores";
+  let equalCount = 0;
+
+
+  //Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho incorreto.".
+  if( number.length != 11 ){
+    return errorCode1;
+  }
+
+  //Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses valores".
+  // Teste se algum dos números é menor que 0 ou maior que 9
+  for ( let index = 0 ; index < number.length; index += 1 ){
+    if( number[index] < 0 || number[index] > 9 ){
+      return errorCode2;
+    }
+  }
+  // Teste se algum dos numeros se repete 3 vezes ou mais
+  for ( let idy = 0; idy < number.length ; idy += 1 ){
+    equalCount = 0;
+    for ( let index = 0; index < number.length; index += 1 ){
+      let comparedNumber = number[idy];
+      if ( comparedNumber === number[index] ){
+        equalCount += 1;
+        if( equalCount >= 3 ){
+          return errorCode2;
+        }
+      }
+    }
+  }
+
+
+  // Começa montando o inicio ()
+  numberResult.push('(' + number[0] + number[1] + ') ' + number[2] + number[3] + number[4] + number[5] + number[6] + '-' + number[7] + number[8] + number[9] + number[10] );
+  myVar = numberResult.toString();
+
+  return myVar;
 }
+
+
+
 
 // Desafio 12
 function triangleCheck() {
