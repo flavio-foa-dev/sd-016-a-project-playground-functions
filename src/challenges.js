06febe26ea4a1a90bc310b3bc3447f5b44377d2b
aparecidag365 <mgegoulart365@gmail.com>
2021-09-02 01:26:20 -0300

    Desafio 7 e alterações  desafio 4 e 6

--- src/challenges.js
+++ src/challenges.js
@@ -2,10 +2,12 @@
 function compareTrue(number1, number2) {
   // Compara dois valores se ambos são true;
   // Retorna com true se caso ambos sejam true e false para qualquer valor # true
+  let number1 = true;
+  let number2 = false;
   if (number1 === true && number2 === true) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
 }
 
@@ -20,8 +22,9 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(stringSplit) {
-  // função recebe uma string 
-  // retorna uma array de strings separadas por cada espaço na string original.  let space = ' ';
+  // função recebe uma string
+  // retorna uma array de strings separadas por cada espaço na string original.
+  // let space = ' ';
   let arrayString = stringSplit.split(space);
   return arrayString
 }
@@ -32,13 +35,14 @@ function concatName(stringArray) {
   // retorna uma string com o formato `'ÚLTIMO ITEM, PRIMEIRO ITEM'`
   // independente do tamanho da array
   let lastIndex = stringArray.length - 1;
-  let stringConcat = stringArray[lastIndex] + ' ' + stringArray[0];
+  let stringConcat = stringArray[lastIndex] + stringArray[0];
   return stringConcat
 }
 
 // Desafio 5
 function footballPoints(wins,ties) {
-  // função recebe o número de vitórias(chamar `wins`) e o número de empates( chamar `ties`)
+  // função recebe o número de vitórias(chamar `wins`)
+  // e o número de empates( chamar `ties`)
   // retorna a quantidade de pontos que o time marcou em um campeonato.
   let spotsTime = (wins * 3) + ties;
   return spotsTime
@@ -51,6 +55,7 @@ function highestCount(numbersArray) {
   // let numbersArray = [0, 0, 0, 1, 1, 1, 1]; //[9, 1, 2, 3, 9, 5, 7] ; //[0, 4, 4, 4, 9, 2, 1]
   let higherAtual  = 0;
   let sumhigherNumber = 0;
+  
   for (let index=0; index <numbersArray.length; index+=1){ 
     let number1 = numbersArray[index];
     let number2 = numbersArray[index+1];
@@ -60,18 +65,44 @@ function highestCount(numbersArray) {
     if ((number2 > number1) && (number2 > higherAtual)){
       let higherAtual = number2;
     }
-  }  
-  for (let Higherindex=0; Higherindex <numbersArray.length; Higherindex+=1){
-    if (numbersArray[Higherindex] === higherAtual) {
-      let sumhigherNumber = sumhigherNumber + 1
+  }
+
+  for (let higherIndex=0; higherIndex <numbersArray.length; higherIndex+=1){
+    if (numbersArray[higherIndex] === higherAtual) {
+      let sumhigherNumber = sumhigherNumber + 1;
     }
-  }  
+  }
    return sumhigherNumber
  }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  /* let cat2 = 2; */
+  /* let cat1 = 3; */
+  /* let mouse = 5; */
+  let positionCat1 = (mouse - cat1);
+  let positionCat2 = (mouse - cat2);
+  
+  if (mouse < cat1) {
+    let positionCat1 = (cat1 - mouse);
+    //console.log(positionCat1)
+  }
+
+  if (mouse < cat2) {
+    let positionCat2 = (cat2 - mouse);
+    //console.log(positionCat2);
+  }
+//
+  if (positionCat1===positionCat2) {
+    //console.log('os gatos trombam e o rato foge')
+    return 'os gatos trombam e o rato foge';
+  }else if (positionCat1 < positionCat2) {
+    //console.log('cat1')
+    return 'cat2';
+  }else if (positionCat2 < positionCat1) {
+    //console.log('cat2')
+    return 'cat2';
+  }
 }
 
 // Desafio 8
