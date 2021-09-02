8cf2a6e30fb7a016bdbf6936e483bc21dbc92197
joaovitordantas <joaovitordantasss@hotmail.com>
2021-09-02 14:11:18 -0300

    Desafio 7 pronto

--- src/challenges.js
+++ src/challenges.js
@@ -25,28 +25,36 @@ function splitSentence(frase) {
 // Desafio 4
 function concatName(listaDeNomes) {
   let firstName = listaDeNomes[0]; // índice do primeiro elemento do array.
-  let lastName = listaDeNomes[listaDeNomes.length-1]; // pega o tamanho do array e subtrai um para pegar índice do último item. REFERENCIA: https://medium.com/@rodrigoum/3-maneiras-de-pegar-o-primeiro-e-%C3%BAltimo-elemento-de-um-array-com-javascript-56e92e6bf3f4
-  let concatenacao = [lastName + ", " + firstName]; // juntando o primeiro e o ultimo.
-  concatenacao = concatenacao.toString(""); // retorna como uma string.
-  return concatenacao; 
+  let lastName = listaDeNomes[listaDeNomes.length - 1]; // pega o tamanho do array e subtrai um para pegar índice do último item. REFERENCIA: https://medium.com/@rodrigoum/3-maneiras-de-pegar-o-primeiro-e-%C3%BAltimo-elemento-de-um-array-com-javascript-56e92e6bf3f4
+  let concatenacao = [lastName + ', ' + firstName]; // juntando o primeiro e o ultimo.
+  concatenacao = concatenacao.toString(''); // retorna como uma string.
+  return concatenacao;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let calculoPontuacao = (wins * 3) + (ties * 1);
+  let calculoPontuacao = (wins * 3) + (ties * 1); // calcula o numero de vitorias vezes a pontuacao (3) mais numero de empates vezes a pontuacao (1).
   return calculoPontuacao;
 }
 
-
 // Desafio 6
 function highestCount() {
-  // seu código aqui
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let resultado = 0;
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    resultado = 'cat1';
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
+    resultado = 'cat2';
+  } else if (Math.abs(cat2 - mouse) === Math.abs(cat1 - mouse)) {
+    resultado = 'os gatos trombam e o rato foge';
+  }
+  return resultado;
 }
+
+console.log(catAndMouse(1, 0, 2));
 
 // Desafio 8
 function fizzBuzz() {
