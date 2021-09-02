72bb118576fc3f4991b583f73bdad8e04a2241ce
laert furquin <laert.ff@gmail.com>
2021-09-02 13:16:38 -0300

    correçao desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,18 +1,20 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  if (param1 === true && param2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
 function splitSentence() {
-  // seu código aqui
 }
-
 // Desafio 4
 function concatName() {
   // seu código aqui
