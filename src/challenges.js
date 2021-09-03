ae44193c1bc0b8ae43f59fdbada7d0e93399f611
mtbxp <marcellotb2@gmail.com>
2021-09-03 15:09:27 -0300

    3 primeiros desafios

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,6 @@
 // Desafio 1
+// let num1 = true 
+// let num2 = false
 function compareTrue(num1, num2) {
   if (num1 && num2 === true) {
     return true;
@@ -6,17 +8,27 @@ function compareTrue(num1, num2) {
     return false;
   }
 }
+// console.log(compareTrue(num1, num2))
 
 // Desafio 2
-function calcArea(base, height) {
+// let base = 3
+// let height = 4
+function calcArea(base, height) { 
   let triangleArea = (base * height) / 2;
   return triangleArea;
 }
 
+// console.log(calcArea(base, height))
+
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+
+// let anyString = "Meu nome é Marcello"
+function splitSentence(anyString) {
+  return anyString.split(' ');
 }
+// console.log(splitSentence(anyString));
+// Consulta ao MDN 
+
 
 // Desafio 4
 function concatName() {
