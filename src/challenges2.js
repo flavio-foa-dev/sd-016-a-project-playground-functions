7209e4b8d0e793648e7ffc62856b6fac2a0aad7a
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-02 23:16:09 -0300

    esboços gerais dos desafios 10 e 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,56 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, name) {
+
+  let techsSorted = techs.sort();
+  let array = [];
+  let mensagem = " ";
+  
+  if (techs.length === 0) {
+    mensagem = 'Vazio'
+  } else {
+    for (let index = 0; index <= techsSorted.length; index += 1) {  
+    let elemento = { 
+    tech: techsSorted[index],
+    name: name, } 
+    techsSorted[index] === elemento.tech;
+    array.push(elemento)
+   }
+   return array
+}
+   return mensagem
 }
 
+let arrayTecnologia = [];
+console.log(techList(arrayTecnologia, 'Lucas'))
+let arrayTecnologia2 = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+console.log(techList(arrayTecnologia2, 'Lucas'));
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeros) {
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lado1,lado2,lado3) {
+
+ 
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
-}
+function hydrate(string) {
+  // baseado e adaptado dos códigos na seguinte página
+  // https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994
+      
+  let contagem = string.match(/[0-9]+/g); 
+  let contadorBedidas = 0;
+           
+  for (let i = 0; i < contagem.length; i += 1 ){
+      contadorBedidas = contadorBedidas + contagem[i];
+  } 
+  return contadorBedidas
+  }
+  console.log(hydrate("8 cervejas e 1 cachaça"));
+
 
 module.exports = {
   generatePhoneNumber,
