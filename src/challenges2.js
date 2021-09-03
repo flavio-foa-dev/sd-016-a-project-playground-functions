2803c4e90225c77783d6e0627b97b680f8cf71e3
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-03 12:58:23 -0300

    desafio 13 hydrate soluçao (?)

--- src/challenges2.js
+++ src/challenges2.js
@@ -32,22 +32,44 @@ function generatePhoneNumber(numeros) {
 
 // Desafio 12
 function triangleCheck(lado1,lado2,lado3) {
-
- 
+    function check1 (lado1, lado2, lado3){
+    let condicao1 = false;
+    if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2) {
+      condicao1 === true;
+    } else {
+      condicao1 === false;
+    }
+    return condicao1
+  }
+  function check2(lado1, lado2, lado3) {
+    let condicao2 = false;
+    if (lado1 > Math.abs(lado2 - lado3) && lado2 > Math.abs(lado1 - lado3) && lado3 > Math.abs(lado1 - lado2)) {
+      condicao2 === true;
+    } else {
+      condicao2 === false;
+    }
+    return condicao2;
+  } 
+  if (check1 === true && check2 === true) {
+    let resultado = true;
+    resultado === true;
+  } else {
+    resultado === false;
+  }
+  return resultado;
 }
 
+console.log(triangleCheck(16, 9, 2));
+
 // Desafio 13
-function hydrate(string) {
-  // baseado e adaptado dos códigos na seguinte página
-  // https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994
-      
+function hydrate(string) {     
   let contagem = string.match(/[0-9]+/g); 
   let contadorBedidas = 0;
            
   for (let i = 0; i < contagem.length; i += 1 ){
-      contadorBedidas = contadorBedidas + contagem[i];
+      contadorBedidas += parseInt(contagem[i]);
   } 
-  return contadorBedidas
+  return contadorBedidas + " copos de água"
   }
   console.log(hydrate("8 cervejas e 1 cachaça"));
 
