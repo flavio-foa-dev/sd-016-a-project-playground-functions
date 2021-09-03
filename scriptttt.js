97bb42ccbe36b491b1bcceec9455d44cb0b3896a
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-09-03 13:13:02 -0300

    Requisito 12

--- scriptttt.js
+++ scriptttt.js
@@ -1,39 +1,19 @@
-function generatePhoneNumber(numbers) {
-  let count = 0;
-  let countRepeat = 0;
-  for (let index in numbers){
-    for (let index3 in numbers){
-    if (numbers[index3] > 9 || numbers[index3] < 0){
-      return "não é possível gerar um número de telefone com esses valores"
-    }else if (numbers.length !== 11){
-      return "Array com tamanho incorreto."
-    }
-    }
-      let verific = numbers[index]
-      for(let index2 in numbers){
-          if (verific === numbers[index2]){
-              count +=1
-          }
-  
-  if (count > countRepeat){
-     countRepeat = count
-  }
-}
-  count = 0;
+function triangleCheck(lineA, lineB, lineC) {
+ let absoluteAB = Math.abs(lineA - lineB)
+ let absoluteAC = Math.abs(lineA - lineC)
+ let absoluteBC = Math.abs(lineB - lineC)
+
+ let sumAB = lineA + lineB
+ let sumAC = lineA + lineC
+ let sumBC = lineB + lineC
+
+ if ((lineA < sumBC || lineB < sumAC || lineC < sumAB ) && (lineA > absoluteBC && lineB > absoluteAC && lineC > absoluteAB)){
+   return true;
+ }
+ else{
+   return false;
+ }
+
 }
 
-if (countRepeat >= 3){
-  return "não é possível gerar um número de telefone com esses valores"
-}else if (numbers.length !== 11){
-  return "Array com tamanho incorreto." }
-  else{
-  
-    numbers.unshift('(')
-    numbers.splice(3, 0, ')')
-    numbers.splice(4, 0, ' ')
-    numbers.splice(10, 0, '-')
-   return numbers.join('')
-  }
-}
-
-console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,-1]))
+console.log(triangleCheck(10, 14, 8))
--- src/challenges2.js
+++ src/challenges2.js
@@ -55,10 +55,23 @@ if (countRepeat >= 3){
   }
 }
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
-
+function triangleCheck(lineA, lineB, lineC) {
+  let absoluteAB = Math.abs(lineA - lineB)
+  let absoluteAC = Math.abs(lineA - lineC)
+  let absoluteBC = Math.abs(lineB - lineC)
+ 
+  let sumAB = lineA + lineB
+  let sumAC = lineA + lineC
+  let sumBC = lineB + lineC
+ 
+  if ((lineA < sumBC || lineB < sumAC || lineC < sumAB ) && (lineA > absoluteBC && lineB > absoluteAC && lineC > absoluteAB)){
+    return true;
+  }
+  else{
+    return false;
+  }
+ 
+ }
 // Desafio 13
 function hydrate() {
   // seu código aqui
