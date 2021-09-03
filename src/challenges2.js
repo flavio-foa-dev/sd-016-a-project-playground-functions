f9f77cd91df72d4ed6ea7596ea5fbc606d5388f8
Marcelo Oliveira <celocmoliveira@gmail.com>
2021-09-03 13:21:16 -0300

    Feito o desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) { // Ajuda do Leandro Leão turma 16 tribo A
+  let ordenada = tech.sort();
+  let arr = [];
+  
+  if (tech.length > 0){
+    for (key in ordenada){
+      let objeto = {
+        tech: ordenada[key],
+        name: name,
+      } 
+     arr.push(objeto) 
+    }
+    return arr;
+  }
+  
+  else {
+    return "Vazio!"
+  }
+  
 }
 
 // Desafio 11
