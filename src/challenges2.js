d4828a99be870ccd55e18010c560b777f76afd15
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-02 22:10:49 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+//10.Crie uma função que recebe um array de nomes de tecnologias que você quer aprender. 
+//Essa função deve receber também um segundo parâmetro chamado name com um nome.
+// Exercicio feito em conjunto com Fumagalli, Priscila, Brunão, Julia e Fabiana
+
+let arrayTechs = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+let nome = "Cris"
+function techList(arrayTechs, name) {
+  let arrayTechsSort = [];
+  let arrayTechsObject = [];
+  let techs;
+  if (arrayTechs == 0) {
+    return "Vazio!"
+  } else {
+    arrayTechsSort = arrayTechs.sort();
+    for (index = 0; index < arrayTechs.length; index += 1) {
+      techs = {
+        tech: arrayTechsSort[index],
+        name: name
+      }
+      arrayTechsObject.push(techs);
+    }
+    return arrayTechsObject;
+  }
 }
+techList(arrayTechs, nome);
 
 // Desafio 11
 function generatePhoneNumber() {
