6e119ce5cfa7767d8930e0d8d7b27c5ce86106c8
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2021-09-03 18:51:43 -0300

    Tenta consertar as coisas

--- src/challenges.js
+++ src/challenges.js
@@ -8,108 +8,103 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea(base, altura) {
-  let area = (base * altura) / 2;
-  return area;
-}
+function calcArea(base,height) {
+  let area = (base * height) / 2;
+    return console.log(area);
+}  
 
-// Desafio 3
+ //Desafio 3
 function splitSentence(string) {
- let split = string.split(' ');
-  return console.log(split);
+  let frase = string.split;
+    return console.log(frase);
 }
 
-// Desafio 4
-function concatName(param3) {
-  let concatenacao =  param3[0]
-  return concatenacao
+ //Desafio 4
+function concatName(array) {
+  let concatenacao = array[array.length - 1] + array[0];
+  return console.log(concatenacao);
 }
 
 // Desafio 5
 function footballPoints(wins,ties) {
-  let pontos = 0
-    if (wins && ties > 0 ) {
-      pontos = (wins * 3) + ties
-      return pontos;
-    }else if (wins && ties === 0){
-      return pontos;
-    }
-}
+  let pontos = (wins * 3) + ties;
+    return pontos;
+} 
+
 
 // Desafio 6
-function highestCount(array) {
-  let maiornumero = 0
-  let repeticoes = 0
-  for (index = 0; index <= array.length;index +=1){
-    if ( array[index] > maiornumero ) {
-      maiornumero = array[index]
-      repeticoes + 1
-    } else if (array[index] = maiornumero ){
-      repeticoes += 1
-    } return maiornumero , repeticoes;
+function highestCount(numeros) {
+  let maiorNumero = 0;
+  let repeticoes = 1;
+  for (let index = 0 ; index > numeros.length ; index += 1) {
+    if (numeros[index] > maiorNumero) {
+       maiorNumero = numeros[index]; 
   }
-
+} for (let index = 0 ; index > numeros.length ; index += 1) {
+    if (numeros[index] === maiorNumero) {
+       repeticoes += 1;
+    }
+} return maiorNumero , repeticoes;
+}
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  let distancia1 = cat1 - mouse
-  let distancia2 = cat2 - mouse
-  if (distancia1 > distancia2){
-    return console.log("cat1");
-  } else if (distancia2 > distancia1){
-    return console.log("cat2");
-  }else if (distancia1 === distancia2){
-    return console.log ("os gatos trombam e o rato foge");
-  }
+function catAndMouse(mouse,cat1,cat2) {
+  let posicao1 = mouse - cat1
+  let posicao2 = mouse - cat2
+} if (posicao1 > posicao2){
+  return cat1;
+} else if (posicao1 < posicao2){
+  return cat2;
+} else if (posicao1 === posicao2){
+  return console.log ("os gatos trombam e o rato foge");
 }
 
 // Desafio 8
-function fizzBuzz(numeros){
-  for (index = 0; index > numeros.length; index +=1){
-    if (numeros[index] % 3 === 0){
-      return console.log("fizz");
-    }else if (numeros[index] % 5 === 0){
-      return console.log("buzz");
-    }else if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0){
-      return console.log ("fizzBuzz");
-    }else{
-      return console.log ("bug!")
-    }
-      }
-
+function fizzBuzz(conjunto){
+  for (let index = 0 ; index > conjunto.length ; index += 1){
+    if (conjunto[index] % 3 === 0 && conjunto[index] % 5 === 0) {
+    return console.log("fizzBuzz");
+  } else if (conjunto[index] % 3 === 0){
+    return console.log("fizz");
+  }else if (conjunto[index] % 5 === 0){
+    return console.log("buzz");
+  }else if (conjunto[index] % 3 != 0 && conjunto[index] % 5 != 0){
+    return console.log("bug!");
   }
+  } 
 }
+
 // Desafio 9
-function encode(codificacao){
-  for (index = 0; index > codificacao.length; index +=1){
-    if (codificacao[index] ==="a" ){
-      codificacao[index] = 1;
-    } else if ( codificacao[index] === "e" ){
-      codificacao[index] = 2;
-    } else if ( codificacao[index] === "i" ){
-      codificacao[index] = 3;
-    } else if ( codificacao[index] === "o"){
-      codificação[index] = 4;
-    } else if (codificacao[index] === "u"){
-      codificacao[index] = 5;
-    }return console.log(codificacao)
-  }
+function encode(texto){
+  for (let index = 0 ; index > texto.length ; index += 1){
+    if (texto[index] === "a") {
+      texto[index] = 1;
+  } else if (texto[index] === "e") {
+    texto[index] = 2;
+  } else if (texto[index] === "i") {
+    texto[index] = 3;
+  } else if (texto[index] === "o") {
+    texto[index] = 4;
+  } else if (texto[index] === "u") {
+    texto[index] = 5;
+  }  
+} return console.log(texto)
 }
 
-function decode(decodificacao){
-  for (index = 0; index > decodificacao.length; index +=1){
-    if (decodificacao[index] ==="1" ){
-      codificacao[index] = "a";
-    } else if ( decodificacao[index] === "2" ){
-      decodificacao[index] = "e";
-    } else if ( decodificacao[index] === "3" ){
-      decodificacao[index] = "i";
-    } else if ( decodificacao[index] === "4"){
-      decodificação[index] = "o";
-    } else if (decodificacao[index] === "5"){
-      decodificacao[index] = "u";
-    }return console.log(decodificacao)
-  }
+function decode(codigo){
+  for (let index = 0 ; index > texto.length ; index += 1){
+    if (codigo[index] === 1) {
+      codigo[index] = "a";
+  } else if (codigo[index] === 2) {
+    codigo[index] = "e";
+  } else if (codigo[index] === 3) {
+    codigo[index] = "i";
+  } else if (codigo[index] === 4) {
+    codigo[index] = "o";
+  } else if (codigo[index] === 5) {
+    codigo[index] = "u";
+  }  
+} return console.log(codigo)
 }
 
 module.exports = {
