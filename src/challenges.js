d1b314ece3ba97990b881578f41212549a10710c
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-09-02 23:11:01 -0300

    Conclui o desafio 5, 6 e 7

--- src/challenges.js
+++ src/challenges.js
@@ -24,18 +24,62 @@ function concatName(palavras) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let maior = numeros[0];
+  let cont = 0;
+  for (let index = 0; index < numeros.length; index++) {
+    if (numeros[index] > maior) maior = numeros[index];
+  }
+  for (let index = 0; index < numeros.length; index++) {
+    if (numeros[index] === maior) cont++;
+  }
+  return cont;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = 0;
+  let distCat2 = 0;
+  if (cat1 === mouse);
+  else if (cat1 < mouse) {
+    while (cat1 != mouse) {
+      cat1++;
+      distCat1++;
+    }
+  }
+    else {
+      while (cat1 != mouse) {
+        cat1--;
+        distCat1++;
+      }
+    }
+  if (cat2 === mouse);
+  else if (cat2 < mouse) {
+    while (cat2 != mouse) {
+      cat2++;
+      distCat2++;
+    }
+  }
+    else {
+      while (cat2 != mouse) {
+        cat2--;
+        distCat2++;
+      }
+    }
+  if (distCat1 === distCat2) {
+    return "os gatos trombam e o rato foge";
+  }
+  else if (distCat1 > distCat2) {
+    return "cat2";
+  }
+  else {
+    return "cat1";
+  }
 }
 
 // Desafio 8
