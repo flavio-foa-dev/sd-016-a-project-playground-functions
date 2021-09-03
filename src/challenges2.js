1599e7c18a61e269ab0f0dce9fbc8b50acb48f15
Priscila Silva <priscilacds_100@yahoo.com.br>
2021-09-02 22:31:49 -0300

    Adiciona exercicio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,33 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(arrayTechs, name) {
+// Criar uma função que receba um ARRAY com nomes de tecnologias que quero aprender.
+// A função também deve receber um novo pârametro chamado "name" com um nome.
+// Para cada tecnologia no array crie um OBJETO com a seguinte estrutura: 
+//{
+// tech: "NomeTech",
+// name: name
+//}
+// Os novos objetos devem ser incluídos em ordem CRESCENTE.
+// Retornar no final da função a lista de objetos.
+
+  let arrayTechsSort = [];
+  let arrayTechsObject = [];
+  let techs;
+  if (arrayTechs == 0) {
+    return "Vazio!"
+  } else {
+    arrayTechsSort = arrayTechs.sort();
+    for (index = 0; index < arrayTechs.length; index += 1) {
+      techs = {
+        tech: arrayTechsSort[index],
+        name: name
+      }
+      arrayTechsObject.push(techs);
+    }
+    return arrayTechsObject;
+  }
 }
+// Desafio 10 feito com a ajuda da Fumagalli, Julia Barcelos e Brunão da Turma 16 - Tribo A.
 
 // Desafio 11
 function generatePhoneNumber() {
