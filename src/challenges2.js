adb28cc49449f44ee6e75dd652ce2f8427e5ab9c
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-09-03 19:37:01 -0300

    soluçao do desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,15 +18,15 @@ function hydrate(string) {
   let contagem = string.match(/[1-9]+/g); 
   let contadorBedidas = 0;
            
-  for (let i = 0; i <= contagem.length; i += 1 ){
+  for (let i = 0; i < contagem.length; i += 1 ){
   contadorBedidas = contadorBedidas + parseInt(contagem[i]);
-  if (contadorBedidas > 1) {
-    return contadorBedidas + " copos de água";
-  } else { 
-    return contadorBedidas + " copo de água"
   }
-}
-}
+  if (contadorBedidas === 1) {
+    return "1 copo de água";
+  } else { 
+    return contadorBedidas + " copos de água"
+  }
+  }
 
   console.log(hydrate("1 cerveja"));
   console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
