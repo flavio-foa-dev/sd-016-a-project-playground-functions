0be1e9dcc7de9c7f9766ea963da1cb2853691ff4
Fabricio Martins <fabriciocsmartins@gmail.com>
2021-09-03 20:28:40 -0300

    primeiros testes concluidos

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,55 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(param1, param2) {
+  return param1 && param2
+  
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+ return (base*height)/2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  var splitted = string.split(" ")
+  return splitted
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  for(let index = 0; index <= array.length; index += 1) {
+    var concatenatedLast = array[(array.length -1)]
+    var concatenatedFirst = array[0]
+    return concatenatedLast +", " + concatenatedFirst
+  }
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  var totalPoints = (wins*3) + (ties*1)
+  return totalPoints
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  count = 0
+  for (let index = 0; index <= array.length; index += 1) {
+    count += Math.max(array[index])
+  }
+  return count
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if((mouse - cat2) == 2 || (cat2 - mouse) == 2 && (mouse - cat1) == 3 || (cat1 - mouse) == 3) {
+    return "cat2"
+  }
+  else if((mouse - cat2) == 12 || (cat2 - mouse) == 12 && (mouse - cat1) == 6 || (cat1 - mouse) == 6) {
+    return "cat1"
+  }
+  else {
+    return "os gatos trombam e o rato foge"
+  }
 }
 
 // Desafio 8
