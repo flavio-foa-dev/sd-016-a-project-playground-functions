cccea0050c96862a107dd22c4cb0a29449357464
neguinho <victor_maanoel@Outlook.com>
2021-09-03 23:21:56 -0400

    realização te o 5

--- src/challenges.js
+++ src/challenges.js
@@ -8,25 +8,35 @@ function compareTrue(param1, param2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2
+  return area;
 }
-
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
+//Desafio 3
+function splitSentence(word) {
+  let string = word
+  string = string.split(" ")
+  return string;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(listName) {    
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  let allWins = 0;  
+  let allTies = 0
+  for (let cont = 1; cont <= wins; cont += 1){
+      allWins += 3
+  }
+  for(let i = 1; i <= ties; i +=1){
+    allTies += 1
+  }  
+    return allWins + allTies
 }
-
+     
+console.log(footballPoints(14,8))
 // Desafio 6
 function highestCount() {
   // seu código aqui
