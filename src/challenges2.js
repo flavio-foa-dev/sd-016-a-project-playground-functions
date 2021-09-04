a24fc6dfeb9e5b0c732a0f17eda71b6bafb784de
Pedro <pedro.oficial1969@gmail.com>
2021-09-04 18:05:29 -0300

    adiciona requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,44 +1,58 @@
 // Desafio 10
-function techList(/* tech, name */) {
+function techList(arrayTech, name) {
+   if (arrayTech.length === 0) {
+    return "Vazio!"
+  } 
 
-  // Desafio 11
-  function generatePhoneNumber(/* num */) {
-    // se tiver 11 numeros tem que retorna em formato de telefone.
-    // se for diferente deve retorna  "Array com tamanho incorreto."
-    /// caso os numeros seja < que 0 ou se repita 3X ou mais deve retorna "não é possível gerar um número de telefone com esses valores".
-/*     if (num.lengh !== 11) {
-      return "Array com tamanho incorreto."
-    }
-    for (let index = 0; index < num.lengh; index += 1) {
-      if (num[index] < 0 || num[index] > 9 || repect(num[index], num))  {
-        return "não é possível gerar um número de telefone com esses valores"
+arrayTech = arrayTech.sort()
+let array = []
+  for (let i = 0; i < arrayTech.length; i += 1) {
+  let obj = {
+    tech: arrayTech[i],
+    name: name,
+  }
+  array.push(obj)
+  }
+  return array;
+}
+// Desafio 11
+function generatePhoneNumber(/* num */) {
+  // se tiver 11 numeros tem que retorna em formato de telefone.
+  // se for diferente deve retorna  "Array com tamanho incorreto."
+  /// caso os numeros seja < que 0 ou se repita 3X ou mais deve retorna "não é possível gerar um número de telefone com esses valores".
+  /*     if (num.lengh !== 11) {
+        return "Array com tamanho incorreto."
       }
-    }
-    function repect(number, array) {
-      let result = 0;
-      for (let i = 0; i < array.lengh; i += 1) {
-        if (number === array[i]) {
-          result += 1;
+      for (let index = 0; index < num.lengh; index += 1) {
+        if (num[index] < 0 || num[index] > 9 || repect(num[index], num))  {
+          return "não é possível gerar um número de telefone com esses valores"
         }
-      } if ( result >= 3) {
-        return true
-      } else {
-        return false
       }
-    } */
-    // Desafio 12
-    function triangleCheck() {
-      // seu código aqui
-    }
+      function repect(number, array) {
+        let result = 0;
+        for (let i = 0; i < array.lengh; i += 1) {
+          if (number === array[i]) {
+            result += 1;
+          }
+        } if ( result >= 3) {
+          return true
+        } else {
+          return false
+        }*/
+}
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
 
-    // Desafio 13
-    function hydrate() {
-      // seu código aqui
-    }
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
 
-    module.exports = {
-      generatePhoneNumber,
-      techList,
-      hydrate,
-      triangleCheck,
-    };
+module.exports = {
+  generatePhoneNumber,
+  techList,
+  hydrate,
+  triangleCheck,
+};
