89d5af6ea971543661ed50e655e0d8a70ed6cfb7
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-09-04 18:11:03 -0400

    add requisito #10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,37 @@
+/* eslint-disable prefer-template */
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let result = [];
+
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  array.sort(); // sort = ordena o array de acordo com a  pontuação de código unicode.
+
+  if (array.length === 0) return 'Vazio!';
+  for (let i = 0; i < array.length; i += 1) {
+    result.push({
+      tech: array[i],
+      name,
+    });
+  }
+  return result;
 }
 
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
+  // nope
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(a, b, c) {
+  if (a > b + c) return false;
+  if (b > a + c) return false;
+  if (c > a + b) return false;
+  return true;
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+  // ...
 }
 
 module.exports = {
