ee7810220f691c3c4d7b68320a50520dccd4d8c0
IgorHamzi <igorwhamzi@gmail.com>
2021-09-03 23:23:27 -0300

    finaliza desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -76,11 +76,44 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(vogais) {
+  let string = "";
+  for (let index = 0;index < vogais.length;index += 1){
+    if (vogais[index] === 'a'){
+      string += '1';
+    }else if(vogais[index] === 'e'){
+      string += '2';
+    }else if(vogais[index] === 'i'){
+      string += '3';
+    }else if(vogais[index] === 'o'){
+      string += '4';
+    }else if(vogais[index] === 'u'){
+      string += '5';
+    } else {
+      string += vogais[index];
+    }
+  }
+  return string;
 }
-function decode() {
-  // seu código aqui
+
+function decode(numbers) {
+  let string2 = "";
+  for (let index = 0;index < numbers.length;index += 1){
+    if (numbers[index] === '1'){
+      string2 += 'a';
+    }else if(numbers[index] === '2'){
+      string2 += 'e';
+    }else if(numbers[index] === '3'){
+      string2 += 'i';
+    }else if(numbers[index] === '4'){
+      string2 += 'o';
+    }else if(numbers[index] === '5'){
+      string2 += 'u';
+    } else {
+      string2 += numbers[index];
+    }
+  }
+  return string2;
 }
 
 module.exports = {
