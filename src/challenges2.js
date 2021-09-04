73aace3dde567d4ee4dcca7aaaa25fe0283574ce
Pedro <pedro.oficial1969@gmail.com>
2021-09-04 20:13:42 -0300

    adiciona requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,44 +1,45 @@
 // Desafio 10
 function techList(arrayTech, name) {
-   if (arrayTech.length === 0) {
+  if (arrayTech.length === 0) {
     return "Vazio!"
-  } 
-
-arrayTech = arrayTech.sort()
-let array = []
-  for (let i = 0; i < arrayTech.length; i += 1) {
-  let obj = {
-    tech: arrayTech[i],
-    name: name,
   }
-  array.push(obj)
+
+  arrayTech = arrayTech.sort()
+  let array = []
+  for (let i = 0; i < arrayTech.length; i += 1) {
+    let obj = {
+      tech: arrayTech[i],
+      name: name,
+    }
+    array.push(obj)
   }
   return array;
 }
-// Desafio 11
-function generatePhoneNumber(/* num */) {
-  // se tiver 11 numeros tem que retorna em formato de telefone.
-  // se for diferente deve retorna  "Array com tamanho incorreto."
-  /// caso os numeros seja < que 0 ou se repita 3X ou mais deve retorna "não é possível gerar um número de telefone com esses valores".
-  /*     if (num.lengh !== 11) {
-        return "Array com tamanho incorreto."
+
+function generatePhoneNumber(num) {
+  let valor = num;
+  if (valor.length !== 11) {
+    return "Array com tamanho incorreto."
+  }
+  for (let index = 0; index < num.length; index += 1) {
+    if (num[index] < 0 || num[index] > 9 || repect(num[index], num)) {
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+  }
+  function repect(number, array) {
+    let result = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      if (number === array[i]) {
+        result += 1;
       }
-      for (let index = 0; index < num.lengh; index += 1) {
-        if (num[index] < 0 || num[index] > 9 || repect(num[index], num))  {
-          return "não é possível gerar um número de telefone com esses valores"
-        }
-      }
-      function repect(number, array) {
-        let result = 0;
-        for (let i = 0; i < array.lengh; i += 1) {
-          if (number === array[i]) {
-            result += 1;
-          }
-        } if ( result >= 3) {
-          return true
-        } else {
-          return false
-        }*/
+    } if (result >= 3) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+return `(${num[0]}${num[1]}) ${num[2]}${num[3]}${num[4]}${num[5]}${num[6]}-${num[7]}${num[8]}${num[9]}${num[10]}`
 }
 // Desafio 12
 function triangleCheck() {
