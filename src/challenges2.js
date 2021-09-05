fdcffec77d4033baa0329e14d8561df17853947b
Lucas (Junqueira) Lima <lucascavalcantelima21@gmail.com>
2021-09-05 20:32:33 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,22 @@
+const { compareTrue } = require("./challenges");
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(namesOfTechnologies, yName) {
+  let list =  namesOfTechnologies.sort();
+  let ordernedList = [];
+  if (list.length > 0){ 
+  for (let i = 0; i < list.length; i += 1) {
+    ordernedList[i] =  {
+       tech: list[i],
+       name: yName,
+     };
+  }
+   return ordernedList;
+  }
+  else{
+    return "Vazio!";
+  }
+
 }
 
 // Desafio 11
