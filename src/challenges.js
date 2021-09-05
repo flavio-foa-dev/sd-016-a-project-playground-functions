4f18847f023342e48ee0cb49900d0efb3d7d97da
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-09-05 00:53:39 -0300

    Requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-    // seu código aqui
+function splitSentence(string) {
+    let separatedWords = string.split(" ")
+    return separatedWords;
 }
 
 // Desafio 4
