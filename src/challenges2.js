7a298080bb17889609195bbc25864cec8a7f9089
Emerson <eemr3@yahoo.com.br>
2021-09-05 14:27:45 -0300

    Requisito 12 - Condição de existência de um triângulo

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,13 +21,49 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
+// function verficaValorNegativo(arrayNumber) {
+//   let count2 = 0;
+//   for (let index = 0; index < arrayNumber.length; index += 1) {
+//     if (arrayNumber[index] < 0 || arrayNumber[index] > 9) {
+//       count2 += 1;
+//     }
+//   }
+//   return count2;
+// }
+// function teste(arrayNumber) {
+//   let count = 0;
+//   for (let index = 0; index < arrayNumber.length; index += 1) {
+//     if (arrayNumber[index + 1] === arrayNumber[index]) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
 function generatePhoneNumber() {
-  // seu código aqui
+  // arrayNumber = arrayNumber.sort();
+  // let count = teste(arrayNumber);
+  // let count2 = verficaValorNegativo(arrayNumber);
+  // if (arrayNumber.length > 11) {
+  //   return 'Array com tamanho incorreto.';
+  // }
+  // if (count >= 3 || count2 > 0) {
+  //   return 'não é possível gerar um número de telefone com esses valores';
+  // }
+  // return count;
 }
 
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2]));
 // Desafio 12
-function triangleCheck() {
+
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+
+  let result = false;
+  if (Math.abs(lineA + lineB) > lineC && Math.abs(lineB + lineC) > lineA
+    && Math.abs(lineA + lineC) > lineB) {
+    result = true;
+  }
+  return result;
 }
 
 // Desafio 13
