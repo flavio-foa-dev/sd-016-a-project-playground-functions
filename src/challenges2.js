12b510e67bfe3f839c22278c74615442ae62d222
PatrickR00t <patrick.s.falquetti@gmail.com>
2021-09-05 22:28:04 +0500

    versão12.3

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,13 +3,7 @@ function techList() {
 }
 
 // Desafio 11
-function generatePhoneNumber(generate) {
-  let middle = '(' + generate[0] + generate[1] + ') ' + generate[2] + generate[3] + generate[4] + generate[5];
-  let rest = generate[6] +'-'+ generate[7] + generate[8] + generate[9] + generate[10];
-  let fullNumber = middle + rest;
-  if (generate === generate.length) {
-    return fullNumber;
-  }
+function generatePhoneNumber() {
 }
 
 // Desafio 12
@@ -18,17 +12,7 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate(cerveja, cachaca, vinho, agua) {
-  if (cerveja === 1) {
-    agua = 1;
-    return agua + "copo de água";
-  } else if (cachaca === 1 && cerveja === 5 && vinho === 1) {
-    agua = 7;
-    return agua + "copos de água";
-  } else if (cachaca === 1 && cerveja === 5 && vinho === 1) {
-    agua = 7;
-    return agua + "copos de água";
-  }
+function hydrate() {
 }
 
 module.exports = {
