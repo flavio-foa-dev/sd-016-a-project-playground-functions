b15c34ad0eb83d081cff35e81bc4872bb288c493
Zaqueu <zaqueufernandes@gmail.com>
2021-09-05 16:48:03 -0300

    Realizaçao dos requesitos 1 ao 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,65 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(value1, value2) {
+// se ambos os valores forem verdadeiros, devo retornar true
+// se não, devo retornar false
+if (value1 === true && value2 === true){
+return true;
+} else  {
+  return false;
+}
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+ let calculoArea = (base * height) / 2;
+ return calculoArea;
 }
+
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+   let array = string.split(' ')
+
+  // console.log(array)
+  return array
 }
+splitSentence('foguete nao da re')
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+    
+  // console.log(array[0])
+
+  let ultimoElementoDoArray = array.length -1
+
+  // console.log(array[ultimoElementoDoArray])
+
+  // console.log(ultimoElementoDoArray) 
+  let primeiroUltimo = array[ultimoElementoDoArray] +','+' '+ array[0]
+
+  return primeiroUltimo
+
 }
+concatName(['casa', 'mesa', 'cama', 'banho', 'Bruno', 'Trybe', 'Zaqueu'])
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  
+let totalDePontos = (wins * 3) + ties
+return totalDePontos
+
+
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+ // retornar o maior numero e a quantidade de vezes que ele se repete dentro do array
+ // [9, 3, 1, 4, 9, 8, 7]
+ let maiorNumero = array[0]
+for (let index = 0; index < array.length; index += 1) {
+  if (maiorNumero < array[index]) {
+  
+}
 }
 
 // Desafio 7
