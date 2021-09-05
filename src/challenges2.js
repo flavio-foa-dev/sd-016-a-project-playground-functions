74bb4cf96956c85ae4ee17b0186c5d5e2b470fe7
joao <jgsspinelli92@gmail.com>
2021-09-05 15:15:09 -0300

    finaliza desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,21 @@
 // Desafio 10
 function techList(techArray, name) {
-  // seu código aqui
+  let objectArray = [];
+  techArray.sort();
 
+  if (techArray.length === 0) {
+    return "Vazio!";
+  }
+
+  for (let key in techArray) {
+    let techs = {
+      tech: techArray[key],
+      name: name
+    }
+    objectArray.push(techs);
+  }
   
+  return objectArray
 } 
 console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "João"))
 // Desafio 11
