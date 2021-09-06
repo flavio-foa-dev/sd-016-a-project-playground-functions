f27dd58f792ff4866b451e8e30938b6e4bd4c652
PauloSuriani <paulosuriani@gmail.com>
2021-09-06 20:38:17 -0300

    Desafio-12

--- src/challenges2.js
+++ src/challenges2.js
@@ -45,8 +45,14 @@ function generatePhoneNumber(phoneNumber) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if((lineA < (lineB + lineC)) && lineA > Math.abs(lineB - lineC) ) {
+    return true;
+  } else if((lineB < (lineA + lineC)) && lineB > Math.abs(lineA - lineC) ) {
+    return true;
+  } else if((lineC < (lineA + lineB)) && lineC > Math.abs(lineA - lineB) ) {
+    return true;
+  } return false;
 }
 
 // Desafio 13
