afc915fb56b5cb0a81909fadcf84f8d2bc70d498
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-06 11:40:58 -0300

    Resolução do desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -88,15 +88,40 @@ function fizzBuzz(arrayNumbers) {
   }
   return arrayreturn;
 }
-let result = fizzBuzz([2, 15, 7, 9, 45]);
-console.log(result);
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(phrase) {
+  let phraseReturn = phrase;
+  let letterEncode = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  phraseReturn = phraseReturn.replace(/a/g, letterEncode.a);
+  phraseReturn = phraseReturn.replace(/e/g, letterEncode.e);
+  phraseReturn = phraseReturn.replace(/i/g, letterEncode.i);
+  phraseReturn = phraseReturn.replace(/o/g, letterEncode.o);
+  phraseReturn = phraseReturn.replace(/u/g, letterEncode.u);
+  return phraseReturn;
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase) {
+  let phraseReturn2 = phrase;
+  let numberDecode = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  phraseReturn2 = phraseReturn2.replace(/1/g, numberDecode[1]);
+  phraseReturn2 = phraseReturn2.replace(/2/g, numberDecode[2]);
+  phraseReturn2 = phraseReturn2.replace(/3/g, numberDecode[3]);
+  phraseReturn2 = phraseReturn2.replace(/4/g, numberDecode[4]);
+  phraseReturn2 = phraseReturn2.replace(/5/g, numberDecode[5]);
+  return phraseReturn2;
 }
 
 module.exports = {
