ca23503e9b6782146e3358e0d4d8cec6bae96fd1
Lonardow <lonardow_fg@hotmail.com>
2021-09-06 17:30:48 -0300

    desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,22 @@
 // Desafio 10
 function techList(array, name) {
   // seu código aqui
-      let objetoFinal = []
-      if(array.length <= 0){
-        return "Vazio!"
-      }
-      array = array.sort()
-      for (techno of array){
-        objetoFinal.push({tech: techno, name: name,})
-      }
-    return objetoFinal;
+  let objetoFinal = [];
+  if (array.length <= 0) {
+    return 'Vazio!';
+  }
+  array = array.sort();
+  for (techno of array) {
+    objetoFinal.push({tech: techno, name: name, });
+  }
+  return objetoFinal;
 }
 
 // Desafio 11
 function generatePhoneNumber(array) {
   let x = 0;
   let numero = array;
-  for(index = 0; index < numero.length; index+=1){
+  for (index = 0; index < numero.length; index += 1) {
        numero.sort();
       if(numero.length > 11 || numero.length < 11){
         return "numero com tamanho incorreto."
@@ -25,12 +25,11 @@ function generatePhoneNumber(array) {
         }else if (numero[index] < 0 || numero[index] > 9 || x === 1){
         return "não é possível gerar um número de telefone com esses valores"
       }
-
   }
     let ddd ="(" + array[0]+array[1] + ")";
     let firstHalf = " " + array[2]+array[3]+array[4]+array[5]+array[6];
     let secondHalf = "-"+ array[7]+array[8]+array[9]+array[10];
-    return ddd+firstHalf+secondHalf;
+  return ddd+firstHalf+secondHalf;
 }
 
 
@@ -51,7 +50,15 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(str) {
+  let numbers = str.match(/\d+/g).map(Number);
+  let glassOfWater = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+    glassOfWater += numbers[index];
+  }
+  if (glassOfWater === 1) {
+    return `${glassOfWater} copo de água`;
+  } return `${glassOfWater} copos de água`;
   // seu código aqui
 }
 
