b06c704d8864bfd105cfa0d0999406e67ead761d
Leandro Leao <leandro.leao@outlook.com>
2021-09-06 15:48:16 -0300

    Arrumando falhas e erros apontandos no Lint.

--- src/challenges.js
+++ src/challenges.js
@@ -9,37 +9,28 @@ function compareTrue(value1, value2) {
 
 // Desafio 2
 function calcArea(base, height) {
-
- return((base * height)/2);
-
+  return ((base * height) / 2);
 }
 
 // Desafio 3
 function splitSentence(Array) {
   let array01 = Array;
-  let splitSentence = array01.split(/[\s,]+/);
-  
-  return splitSentence;
-
+  let splitSentence01 = array01.split(/[\s,]+/);
+  return splitSentence01;
 }
 
 // Desafio 4
 function concatName(nameConcat) {
   let firstItem = nameConcat.shift();
   let lastItem = nameConcat.pop();
-  let resultContat = lastItem + ', ' + firstItem;
-
-  return resultContat;
-
+  return lastItem + ', ' + firstItem;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   let winPoints = wins * 3;
   let totalPoints = winPoints + ties;
-
   return totalPoints;
-
 }
 
 // Desafio 6
@@ -47,20 +38,18 @@ function highestCount(numberArray) {
   let higherNumber = numberArray[0];
   let higherNumberIndex = 0;
 
-  for (indexHC = 0; indexHC < numberArray.length ; indexHC += 1) {
+  for (let indexHC = 0; indexHC < numberArray.length; indexHC += 1) {
     if (higherNumber < numberArray[indexHC]) {
       higherNumber = numberArray[indexHC];
     }
   }
-  for (indexHI = 0; indexHI < numberArray.length ; indexHI += 1) {
+  for (let indexHI = 0; indexHI < numberArray.length; indexHI += 1) {
     if (higherNumber === numberArray[indexHI]) {
       higherNumberIndex += 1;
     }
-  
   }
   return higherNumberIndex;
 }
-
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -70,75 +59,70 @@ function catAndMouse(mouse, cat1, cat2) {
   let geralPosCat2 = Math.abs(initialPositionCat2);
 
   if (geralPosCat1 > geralPosCat2) {
-    return "cat2";
+    return 'cat2';
   } else if (geralPosCat1 === geralPosCat2) {
-    return "os gatos trombam e o rato foge";
-  }  else {
-    return "cat1";
+    return 'os gatos trombam e o rato foge';
+  } else {
+    return 'cat1';
   }
-
 }
 
 // Desafio 8
 function fizzBuzz(arrayFB) {
   let fizzBuzzResult = [];
 
-  for (let indexFB = 0; indexFB < arrayFB.length ; indexFB += 1) {
-      if ((arrayFB[indexFB] % 3) === 0 && (arrayFB[indexFB] % 5) === 0) {
-      fizzBuzzResult.push("fizzBuzz");
+  for (let indexFB = 0; indexFB < arrayFB.length; indexFB += 1) {
+    if ((arrayFB[indexFB] % 3) === 0 && (arrayFB[indexFB] % 5) === 0) {
+      fizzBuzzResult.push('fizzBuzz');
     } else if ((arrayFB[indexFB] % 3) === 0) {
-      fizzBuzzResult.push("fizz");
+      fizzBuzzResult.push('fizz');
     } else if ((arrayFB[indexFB] % 5) === 0) {
-      fizzBuzzResult.push("buzz");
+      fizzBuzzResult.push('buzz');
     } else {
-      fizzBuzzResult.push("bug!");
+      fizzBuzzResult.push('bug!');
     }
   }
-return (fizzBuzzResult);
-
+  return (fizzBuzzResult);
 }
 
 // Desafio 9
 function encode(arrayEncode) {
   let splitArrayEncode = arrayEncode.split('');
 
-  for ( index = 0; index < splitArrayEncode.length; index += 1) {
-    if (splitArrayEncode[index] === "a") {
-      splitArrayEncode[index] = "1";
-    } else if (splitArrayEncode[index] === "e") {
-      splitArrayEncode[index] = "2";
-    } else if (splitArrayEncode[index] === "i") {
-      splitArrayEncode[index] = "3";
-    } else if (splitArrayEncode[index] === "o") {
-      splitArrayEncode[index] = "4";
-    } else if (splitArrayEncode[index] === "u") {
-      splitArrayEncode[index] = "5";
+  for (let index = 0; index < splitArrayEncode.length; index += 1) {
+    if (splitArrayEncode[index] === 'a') {
+      splitArrayEncode[index] = '1';
+    } else if (splitArrayEncode[index] === 'e') {
+      splitArrayEncode[index] = '2';
+    } else if (splitArrayEncode[index] === 'i') {
+      splitArrayEncode[index] = '3';
+    } else if (splitArrayEncode[index] === 'o') {
+      splitArrayEncode[index] = '4';
+    } else if (splitArrayEncode[index] === 'u') {
+      splitArrayEncode[index] = '5';
     }
   }
-  
-  let joinArrayEncode = splitArrayEncode.join('')
+  let joinArrayEncode = splitArrayEncode.join('');
 
   return joinArrayEncode;
 }
 
 function decode(arrayDecode) {
   let splitArrayDecode = arrayDecode.split('');
-
-  for ( index = 0; index < splitArrayDecode.length; index += 1) {
-    if (splitArrayDecode[index] === "1") {
-      splitArrayDecode[index] = "a";
-    } else if (splitArrayDecode[index] === "2") {
-      splitArrayDecode[index] = "e";
-    } else if (splitArrayDecode[index] === "3") {
-      splitArrayDecode[index] = "i";
-    } else if (splitArrayDecode[index] === "4") {
-      splitArrayDecode[index] = "o";
-    } else if (splitArrayDecode[index] === "5") {
-      splitArrayDecode[index] = "u";
+  for (let index = 0; index < splitArrayDecode.length; index += 1) {
+    if (splitArrayDecode[index] === '1') {
+      splitArrayDecode[index] = 'a';
+    } else if (splitArrayDecode[index] === '2') {
+      splitArrayDecode[index] = 'e';
+    } else if (splitArrayDecode[index] === '3') {
+      splitArrayDecode[index] = 'i';
+    } else if (splitArrayDecode[index] === '4') {
+      splitArrayDecode[index] = 'o';
+    } else if (splitArrayDecode[index] === '5') {
+      splitArrayDecode[index] = 'u';
     }
   }
-  
-  let joinArrayDecode = splitArrayDecode.join('')
+  let joinArrayDecode = splitArrayDecode.join('');
 
   return joinArrayDecode;
 }
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,62 +1,52 @@
 // Desafio 10
 function techList(technologies, name) {
-let techName = technologies;
-techName.sort();
-let objName = name;
-let listArray = [];
+  let techName = technologies;
+  techName.sort();
+  let objName = name;
+  let listArray = [];
 
-if (techName.length > 0) {
-  for (index = 0; index < techName.length; index +=1) {
-    let objeto = {
-      tech: techName[index],
-      name: objName,
+  if (techName.length > 0) {
+    for (let index = 0; index < techName.length; index += 1) {
+      let objeto = {
+        tech: techName[index],
+        name: objName,
+      };
+      listArray.push(objeto);
     }
-  listArray.push(objeto);
-  }
-  return(listArray);
-} else {
-    return "Vazio!"
+    return (listArray);
+  } else {
+    return 'Vazio!';
   }
 }
 
 // Desafio 11
 function generatePhoneNumber(phoneNumber) {
   let phone = phoneNumber;
-  
   // os unicos numeros possíveis no array *Phone* //
-  let possibleNumbers = [0,1,2,3,4,5,6,7,8,9];
-  
+  let possibleNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   // o formato de numero de celular no final. Ex: (11) 98948-3530 //
-  let numberCel = "(" + phone[0] + phone[1] + ") " + phone[2] + phone[3] + phone[4] + phone[5] + phone[6] + "-" + phone[7] + phone[8] + phone[9] + phone[10];
-  
+  let numberCel = '(' + phone[0] + phone[1] + ') ' + phone[2] + phone[3] + phone[4] + phone[5] + phone[6] + '-' + phone[7] + phone[8] + phone[9] + phone[10];
   // Verificar quantas vezes um número se repete no array Phone //
   let repetition = 0;
   let repetitionArray = [];
-  
   // Para verificar se existe mais de 3 digitos no array //
-  for (let test = 0; test < possibleNumbers.length; test += 1 ) {
+  for (let test = 0; test < possibleNumbers.length; test += 1) {
     let positionPossible = possibleNumbers[test];
-    if (repetition > 0){
+    if (repetition > 0) {
       repetition = 0;
     }
     for (let index = 0; index < phone.length; index += 1) {
-    let positionNumero = phone[index];
-  
-        if (positionPossible === positionNumero) {
-          repetition = repetition + 1;
-          }
-        
+      let positionNumero = phone[index];
+      if (positionPossible === positionNumero) {
+        repetition += 1;
+      }
     }
-    repetitionArray.push(repetition);  
+    repetitionArray.push(repetition);
   }
-  
-  // Resultado de 3 numeros se repetindo no array *Phone*. O Resultado esperado para que não haja mais do que três digitos para essa formula é -1 // 
-  let repetitionIndexOf = repetitionArray.indexOf(3)
-  
+  // Resultado de 3 numeros se repetindo no array *Phone*. O Resultado esperado para que não haja mais do que três digitos para essa formula é -1 //
+  let repetitionIndexOf = repetitionArray.indexOf(3);
   // Verificar se existe numero < 0 ou > 9 no array //
-
   let verifyPhone = true;
-
   for (let index = 0; index < phone.length; index += 1) {
     if (phone[index] >= 0 && phone[index] <= 9) {
       verifyPhone = verifyPhone;
@@ -64,23 +54,21 @@ function generatePhoneNumber(phoneNumber) {
       verifyPhone = false;
     }
   }
-
   // IF final para verificar condições e retornar resultado //
-
   if (phone.length > 11 || phone.length < 11) {
-    return ("Array com tamanho incorreto.");
+    return ('Array com tamanho incorreto.');
   } else if (repetitionIndexOf > -1) {
-    return ("não é possível gerar um número de telefone com esses valores");
-  } else if (verifyPhone === false ) {
-      return ("não é possível gerar um número de telefone com esses valores");
+    return ('não é possível gerar um número de telefone com esses valores');
+  } else if (verifyPhone === false) {
+    return ('não é possível gerar um número de telefone com esses valores');
   } else {
-      return (numberCel);
-      }
+    return (numberCel);
+  }
 }
 
 // Desafio 12
 function triangleCheck(side01, side02, side03) {
-  if (side01 < (side02+side03) && side01 > (Math.abs(side02-side03))) {
+  if (side01 < (side02 + side03) && side01 > (Math.abs(side02 - side03))) {
     return true;
   } else {
     return false;
@@ -94,18 +82,15 @@ function hydrate(mainString) {
   let mainArray = mainString.match(regex);
   let mainsArraySum = 0;
 
-  for (let index = 0; index < mainArray.length; index +=1) {
-    mainsArraySum = mainsArraySum + parseInt(mainArray[index]);
+  for (let index = 0; index < mainArray.length; index += 1) {
+    mainsArraySum += parseInt(mainArray[index], 10);
   }
-  
   if (mainsArraySum > 1) {
-    return (mainsArraySum + " copos de água")
-  } else { 
-    return (mainsArraySum + " copo de água")
+    return (mainsArraySum + ' copos de água');
+  } else {
+    return (mainsArraySum + ' copo de água');
   }
-
 }
-
 
 module.exports = {
   generatePhoneNumber,
