90f48da7ea225bb22f1256e16f9e346b4a87c427
Lucas de Oliveira Barboza <lob619@gmail.com>
2021-09-06 16:59:09 -0300

    Requesito 12: check

--- src/challenges2.js
+++ src/challenges2.js
@@ -57,8 +57,11 @@ function generatePhoneNumber(array) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if ((lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) && (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) && (lineC < lineB + lineA && lineC > Math.abs(lineB - lineA))) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 13
