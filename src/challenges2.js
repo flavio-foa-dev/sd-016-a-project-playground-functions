fcccc46a827f08ccb1baeff3af2deb767e028051
willia de OLiveira  Marques <willianmarques_fsa@hotmail.com>
2021-09-05 22:39:08 -0300

    Final de domingo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,56 @@
 // Desafio 10
-function techList() {
+function techList(array, string) {
   // seu código aqui
+  let resultado = [];
+  let result = [];
+  if(array.length<1){
+    resultado = "Vazio!";
+  }
+  for(let i = 0; i < array.length; i=i+1) {
+    result.push(array[i]);
+    result.sort();  
+  }
+  for(let i = 0; i < result.length; i++){
+    resultado.push({tech:result[i], name:string})  
+  }
+  return resultado   
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let resultado 
+  let arr = [];
+   if(array.length!==11){
+    resultado = "Array com tamanho incorreto.";
+    return resultado;
+  } 
+if(array.length==11) {
+  for(let i = 0; i < array.length; i=i+1){
+    if(array[i]>9||array[i]<0){
+      
+    resultado = "não é possível gerar um número de telefone com esses valores";
+    return resultado;
+    }
+  
+  }
+  for(let i = 2; i < 7; i=i+1){
+  arr.push(array[i]);
+  }
+  arr.push("-")  
+  for(let i = 7; i < array.length; i=i+1){
+  arr.push(array[i]);
+  }
+  resultado =  "("+array[0]+array[1]+")" +" "+ arr.join('')
+   
+  return resultado
 }
+
+
+  
+
+}
+console.log(generatePhoneNumber([1,2,3,2,2,3,3,8,5,6,0]))
 
 // Desafio 12
 function triangleCheck() {
