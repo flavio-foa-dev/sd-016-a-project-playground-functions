d5d41ea91bf0c98c589e712bcf65fc2c82a5dce2
arctavares <cosilk@hotmail.com>
2021-09-06 15:56:47 -0300

    Resolve dois erros lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -62,11 +62,26 @@ function generatePhoneNumber(array) {
 }
 
 // Desafio 12
+
+function checkSide(lineA, lineB, lineC) {
+  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
+    return true;
+  }
+  return false;
+}
+
 function triangleCheck(lineA, lineB, lineC) {
-  if (((lineA < lineB + lineC)
-    && (lineB < lineA + lineC) && (lineC < lineA + lineB))
-    && ((lineA > Math.abs(lineB - lineC) && (lineB > Math.abs(lineA - lineC)
-      && (lineC > Math.abs(lineA - lineB)))))) {
+  let count = 0;
+  if (checkSide(lineA, lineB, lineC) === true) {
+    count += 1;
+  }
+  if (checkSide(lineB, lineA, lineC) === true) {
+    count += 1;
+  }
+  if (checkSide(lineC, lineA, lineB) === true) {
+    count += 1;
+  }
+  if (count === 3) {
     return true;
   }
   return false;
@@ -75,11 +90,10 @@ function triangleCheck(lineA, lineB, lineC) {
 // Desafio 13
 
 function match(string) {
+  let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let matches = [];
   for (let i = 0; i < string.length; i += 1) {
-    if (string[i] === '1' || string[i] === '2' || string[i] === '3'
-      || string[i] === '4' || string[i] === '5' || string[i] === '6'
-      || string[i] === '7' || string[i] === '8' || string[i] === '9') {
+    if (numbers.includes(string[i]) === true) {
       matches.push(string[i]);
     }
   }
@@ -95,7 +109,7 @@ function hydrate(string) {
   matches = match(string);
 
   for (let i = 0; i < matches.length; i += 1) {
-    sum += parseInt(matches[i], 10); // -> sem o 10 dá erro no lint, o segundo numero significa a base que nesse caso é 10(decimal)
+    sum += parseInt(matches[i], 10); // -> sem o 10 dá erro no lint, o segundo parametro significa a base que nesse caso é 10(decimal)
     // passa de string para number -> https://www.alura.com.br/artigos/convertendo-string-para-numero-em-javascript?gclid=Cj0KCQjwpreJBhDvARIsAF1_BU2BKYU6f6ReQl7b1z1TH6HDzqJ7xsHremu8uaTEcvL1hkzuWM9gOjYaAh1DEALw_wcB
   }
   if (sum === 1) {
