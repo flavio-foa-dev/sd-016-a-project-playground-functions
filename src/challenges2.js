1d4daf8e459bc46e296f486658cdb2b1abb55f7c
morkertt <andremorkertt@hotmail.com>
2021-09-05 22:04:15 -0300

    [ADD] Desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,27 +1,49 @@
 // Desafio 10
-function techList(tech, name) {
-// recebe array de nomes de tec e um nome
-  tech = tech.sort();
-  let ret = {};
+function techList(technology, student) {
+  technology = technology.sort();
   let arr = [];
-  if (tech.length <= 0) {
-    return "Vazio!"
-  } else {
-  for (let i in tech) {
-    arr[i] = {"tech": tech[i], "name": name}
-    }
-    return arr
-  }  
+  if (technology.length <= 0) {
+    return 'Vazio!';
+  }
+  for (let i = 0; i < technology.length; i += 1) {
+    arr[i] = { tech: technology[i], name: student };
+  }
+  return arr;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(a) {
+  let tmp = a.slice();
+  let count = 1;
+  if (a.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  tmp.sort();
+  for (let i = 0; i < a.length; i += 1) {
+    if (tmp[i] === tmp[i + 1]) {
+      count += 1;
+    } else { count = 1; }
+    if (a[i] < 0 || a[i] > 9 || count > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let add = a.join('');
+  let ddd = `(${add.substring(0, 2)}) `;
+  let num1 = `${add.substring(2, 7)}-`;
+  // let ddd = '(' + add.substring(0, 2) + ') ';
+  // let num1 = add.substring(2, 7) + '-';
+  let num2 = add.substring(7, 11);
+  return ddd + num1 + num2;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let number = [lineA, lineB, lineC];
+  number = number.sort((a, b) => a - b).reverse('');
+  let absolute = number[0] - number[1] - number[2];
+  if (number[0] < number[1] + number[2] && number[0] > Math.abs(absolute)) {
+    return true;
+  } return false;
 }
 
 // Desafio 13
