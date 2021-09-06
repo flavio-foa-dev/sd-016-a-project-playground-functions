630f406fc911aeb8389a60602a27587a989891e4
Christian Lessa <christianclessa@gmail.com>
2021-09-06 15:07:47 -0300

    Requisito 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,38 @@ function techList(arrTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numerosTel) {
+  let vezesNumero = 0;
+  let deuCerto = true;
+
+  if (numerosTel.length !== 11) {
+    deuCerto = false;
+    return "Array com tamanho incorreto.";
+  } else {
+      for (let index = 0; index < numerosTel.length; index += 1) {
+        if (numerosTel[index] < 0 || numerosTel[index] > 9) {
+          deuCerto = false;
+          return "não é possível gerar um número de telefone com esses valores"
+        }
+      }
+        for (let index = 0; index < numerosTel.length; index += 1) {
+            vezesNumero = 0;
+          for (let segundoIndex = 0; segundoIndex < numerosTel.length; segundoIndex += 1) {
+            if (numerosTel[index] === numerosTel[segundoIndex]) {
+              vezesNumero = vezesNumero + 1;
+              if (vezesNumero >= 3) {
+                deuCerto = false;
+                return "não é possível gerar um número de telefone com esses valores"
+              }
+            }
+          }
+        } 
+  }
+      if (deuCerto === true) {
+        let ddd = "(" + numerosTel[0] + numerosTel[1] + ") " + numerosTel[2] + numerosTel[3] + numerosTel[4] + numerosTel[5] + numerosTel[6] + "-" + numerosTel[7] + numerosTel[8] + numerosTel[9] + numerosTel[10]; 
+
+        return ddd
+      }    
 }
 
 // Desafio 12
