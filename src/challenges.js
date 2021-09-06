e2396fb3170d191b840ca08d8d7c6672fb6c989e
IgorHamzi <igorwhamzi@gmail.com>
2021-09-06 10:18:50 -0300

    corrige 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,15 @@
 // Desafio 1
-function compareTrue(a,b) {
-  if(a === true && b === true) {
-    return true;
-  }else{
-    return false;
+function compareTrue(a, b) {
+  let result = false;
+  if (a === true && b === true) {
+    result = true;
   }
+  return result;
 }
 
 // Desafio 2
-function calcArea(base,heigth) {
-  let area = (base * heigth) /2;
+function calcArea(base, heigth) {
+  let area = (base * heigth) / 2;
   return area;
 }
 
@@ -20,25 +20,25 @@ function splitSentence(sentence) {
 
 // Desafio 4
 function concatName(words) {
-  let last = words[(words.length -1)]
+  let last = words[(words.length - 1)];
   let first = words[0];
   return last + ", " + first;
 }
 
 // Desafio 5
-function footballPoints(wins,ties) {
-  return (wins * 3) + ties
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
 }
 
 // Desafio 6
 function highestCount(numbers) {
   let maior = numbers[0];
   let soma = 0;
-  for(let index = 0;index < numbers.length;index += 1){
-    if(maior < numbers[index]) {
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (maior < numbers[index]) {
       maior = numbers[index];
-      soma = 1
-    }else if (maior === numbers[index]){
+      soma = 1;
+    } else if (maior === numbers[index]) {
       soma += 1;
     }
   }
@@ -46,30 +46,30 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse(mouse,cat1,cat2) {
+function catAndMouse(mouse, cat1, cat2) {
   let distCat1 = Math.abs(mouse - cat1);
   let distCat2 = Math.abs(mouse - cat2);
-  if(distCat1 < distCat2){
+  if (distCat1 < distCat2) {
     return 'cat1';
-  } else if(distCat1 > distCat2){
+  } else if (distCat1 > distCat2) {
     return 'cat2';
   } else {
     return 'os gatos trombam e o rato foge';
-  }  
+  }
 }
 
 // Desafio 8
 function fizzBuzz(numbers) {
   let result = [];
-  for (let index = 0;index < numbers.length;index += 1){
-    if ((numbers[index] % 3 === 0) && (numbers[index] % 5 != 0)){
-      result.push("fizz");
-    }else if((numbers[index] % 5 === 0) && (numbers[index] % 3 != 0)){
-      result.push("buzz");
-    }else if((numbers[index] % 3 === 0) && (numbers[index] % 5 === 0)){
-      result.push("fizzBuzz");
-    }else{
-      result.push("bug!");
+  for (let index = 0; index < numbers.length; index += 1) {
+    if ((numbers[index] % 3 === 0) && (numbers[index] % 5 !== 0)) {
+      result.push('fizz');
+    } else if ((numbers[index] % 5 === 0) && (numbers[index] % 3 !== 0)) {
+      result.push('buzz');
+    } else if ((numbers[index] % 3 === 0) && (numbers[index] % 5 === 0)) {
+      result.push('fizzBuzz');
+    } else {
+      result.push('bug!');
     }
   }
   return result;
@@ -77,17 +77,17 @@ function fizzBuzz(numbers) {
 
 // Desafio 9
 function encode(vogais) {
-  let string = "";
-  for (let index = 0;index < vogais.length;index += 1){
-    if (vogais[index] === 'a'){
+  let string = '';
+  for (let index = 0; index < vogais.length; index += 1) {
+    if (vogais[index] === 'a') {
       string += '1';
-    }else if(vogais[index] === 'e'){
+    } else if (vogais[index] === 'e') {
       string += '2';
-    }else if(vogais[index] === 'i'){
+    } else if (vogais[index] === 'i') {
       string += '3';
-    }else if(vogais[index] === 'o'){
+    } else if (vogais[index] === 'o') {
       string += '4';
-    }else if(vogais[index] === 'u'){
+    } else if (vogais[index] === 'u') {
       string += '5';
     } else {
       string += vogais[index];
@@ -97,17 +97,17 @@ function encode(vogais) {
 }
 
 function decode(numbers) {
-  let string2 = "";
-  for (let index = 0;index < numbers.length;index += 1){
+  let string2 = '';
+  for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] === '1'){
       string2 += 'a';
-    }else if(numbers[index] === '2'){
+    } else if (numbers[index] === '2') {
       string2 += 'e';
-    }else if(numbers[index] === '3'){
+    } else if (numbers[index] === '3') {
       string2 += 'i';
-    }else if(numbers[index] === '4'){
+    } else if (numbers[index] === '4') {
       string2 += 'o';
-    }else if(numbers[index] === '5'){
+    } else if (numbers[index] === '5') {
       string2 += 'u';
     } else {
       string2 += numbers[index];
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let dev = {
+    tech : "",
+    name : "" 
+  }
+
+  let result = [];
+
+  for (let index = 0; index < tech.length; index += 1) {
+    let objeto = dev[tech[index], name];
+    console.log(objeto.tech);
+    result.push(objeto);
+  }
+  return result;
 }
+
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
+
 
 // Desafio 11
 function generatePhoneNumber() {
