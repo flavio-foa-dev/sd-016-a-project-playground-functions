4609d56a05d9610c0c0545bbafe7f7797af73a2d
joao <jgsspinelli92@gmail.com>
2021-09-05 23:16:47 -0300

    resolve mais 4 problemas do lint

--- src/challenges.js
+++ src/challenges.js
@@ -54,12 +54,12 @@ function catAndMouse(mouse, cat1, cat2) {
 function fizzBuzz(numbersArray) {
   let namesArray = [];
   for (let index = 0; index < numbersArray.length; index += 1) {
-    if (numbersArray[index] % 3 === 0 && numbersArray[index] % 5 !== 0) {
-      namesArray.push('fizz');
-    } else if (numbersArray[index] % 5 === 0 && numbersArray[index] % 3 !== 0) {
-      namesArray.push('buzz');
-    } else if (numbersArray[index] % 3 === 0 && numbersArray[index] % 5 === 0) {
+    if (numbersArray[index] % 3 === 0 && numbersArray[index] % 5 === 0) {
       namesArray.push('fizzBuzz');
+    } else if (numbersArray[index] % 5 === 0) {
+      namesArray.push('buzz');
+    } else if (numbersArray[index] % 3 === 0) {
+      namesArray.push('fizz');
     } else {
       namesArray.push('bug!');
     }
@@ -67,43 +67,15 @@ function fizzBuzz(numbersArray) {
   return namesArray;
 }
 // Desafio 9
-function encode(message) {
-  let encodedArray = [];
-  for (let key of message) {
-    if (key === 'a') {
-      encodedArray.push('1');
-    } else if (key === 'e') {
-      encodedArray.push('2');
-    } else if (key === 'i') {
-      encodedArray.push('3');
-    } else if (key === 'o') {
-      encodedArray.push('4');
-    } else if (key === 'u') {
-      encodedArray.push('5');
-    } else {
-      encodedArray.push(key);
-    }
-  }
-  return encodedArray.join('');
+function encode(string) {
+  let newString = string.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4')
+    .replace(/u/g, '5');
+  return newString;
 }
-function decode(message) {
-  let decodedArray = [];
-  for (let key of message) {
-    if (key === '1') {
-      decodedArray.push('a');
-    } else if (key === '2') {
-      decodedArray.push('e');
-    } else if (key === '3') {
-      decodedArray.push('i');
-    } else if (key === '4') {
-      decodedArray.push('o');
-    } else if (key === '5') {
-      decodedArray.push('u');
-    } else {
-      decodedArray.push(key);
-    }
-  }
-  return decodedArray.join('');
+function decode(string) {
+  let newString = string.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+  return newString;
 }
 module.exports = {
   calcArea,
