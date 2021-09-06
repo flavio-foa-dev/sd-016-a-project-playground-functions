f540365d3e1a97d0c5425a0fa66ecd9f620e0b90
Veidz <joaovictorveidz@gmail.com>
2021-09-06 04:14:58 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
+function techList(array, string) {
   // seu código aqui
+  let orderedArray = array.sort();
+  let newArray = [];
+  
+  if (array.length > 0) {
+    for (let key in orderedArray) {
+      let object = {
+        tech: orderedArray[key],
+        name: string
+      }
+      newArray.push(object);
+    }
+    return newArray;
+  } else {
+    return 'Vazio!';
+  }
 }
 
 // Desafio 11
