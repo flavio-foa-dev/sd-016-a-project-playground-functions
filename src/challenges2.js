2c8c96dc48288ab65f396a2a53d45fd1a2ece16f
PauloSuriani <paulosuriani@gmail.com>
2021-09-06 20:25:00 -0300

    Desafio-11

--- src/challenges2.js
+++ src/challenges2.js
@@ -15,8 +15,33 @@ function techList(tecnologias, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  if(phoneNumber.length !== 11) {
+    return("Array com tamanho incorreto.");
+  }
+  let formatedNumber = '(';
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    if (phoneNumber[index] > 9 || phoneNumber[index] < 0) {
+      return ("não é possível gerar um número de telefone com esses valores");
+    }
+    let aux = phoneNumber[index];
+    let count = 0;
+    for (let subIndex = 0; subIndex < phoneNumber.length; subIndex += 1) {
+      if (aux === phoneNumber[subIndex]) {
+        count += 1;
+      }
+    } 
+    if (count >= 3) {
+      return ("não é possível gerar um número de telefone com esses valores");
+    }
+    formatedNumber += phoneNumber[index];
+    if (index === 1) {
+      formatedNumber += ') ';
+    } else if (index === 6){
+      formatedNumber += '-';
+    }
+  }
+  return formatedNumber;
 }
 
 // Desafio 12
