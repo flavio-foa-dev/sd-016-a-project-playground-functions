f74e32161b0886aadc65523cfb28d28a3e68cc34
jonmanhaes <jonatasjonasjonat@gmail.com>
2021-09-06 15:16:45 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -62,8 +62,21 @@ function catAndMouse(mouse, cat1, cat2) {
   } 
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let result = [];
+  for(let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+      result.push("fizzBuzz")
+    } else if (numbers[i] % 5 === 0) {
+      result.push("buzz")
+    } else if (numbers[i] % 3 === 0) {
+      result.push("fizz")
+    } else {
+      result.push("bug!")
+    }
+
+  }
+  return result
 }
 
 // Desafio 9
