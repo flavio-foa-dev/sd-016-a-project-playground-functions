a50c1e01e8e0adb3e5a8a40ebc64ff2d85991043
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-09-06 02:01:09 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -53,8 +53,16 @@ function highestCount(numberList) {
 }
 
 // Desafio 7
-function catAndMouse() {
-    // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+    let distanceCat1 = cat1 - mouse;
+    let distanceCat2 = cat2 - mouse;
+    if (Math.abs(distanceCat1) < Math.abs(distanceCat2)) {
+        return "cat1";
+    } else if (Math.abs(distanceCat1) > Math.abs(distanceCat2)) {
+        return "cat2";
+    } else if (Math.abs(distanceCat1) === Math.abs(distanceCat2)) {
+        return "os gatos trombam e o rato foge";
+    }
 }
 
 // Desafio 8
