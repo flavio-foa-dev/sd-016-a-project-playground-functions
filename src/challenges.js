763c36f1e703fb6cb6adab87664101bfaa32556f
Eric Vinícius <ericvinicius81@gmail.com>
2021-09-07 11:09:06 -0300

    Desafio 7 com correçao automatica do ESLint no VS Code

--- src/challenges.js
+++ src/challenges.js
@@ -49,14 +49,12 @@ function highestCount(numbers) {
 function catAndMouse(mouse, cat1, cat2) {
   let distance1 = cat1 - mouse;
   let distance2 = cat2 - mouse;
-
-  if ((distance1) == (distance2) || (distance1 *(-1)) == (distance2) || (distance2 * (-1)) == (distance1)) {
+  if ((distance1) === (distance2) || (distance1 * (-1)) === (distance2)
+  || (distance2 * (-1)) === (distance1)) {
     return 'os gatos trombam e o rato foge';
-  } else if (distance1 < distance2) {
+  } if (distance1 < distance2) {
     return 'cat1';
-  } else {
-    return 'cat2';
-  }
+  } return 'cat2';
 }
 
 // Desafio 8
