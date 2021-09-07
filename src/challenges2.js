405f78e1a1161abf166654f01104f8ba152c7328
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-09-07 19:28:00 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,41 @@ function techList(arrayTechNames, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayNumbers) {
   // seu código aqui
+  let arrayPhone = [];
+
+  if (arrayNumbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    let count = 0;
+    if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let secondIndex = 0; secondIndex < arrayNumbers.length; secondIndex += 1 ) {
+      if (arrayNumbers[index] === arrayNumbers[secondIndex]) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';  
+    }    
+    if (index === 0) {
+      let aux = '(';
+      arrayPhone.push(aux);  
+    }
+    if (index === 2) {
+      let aux = ')' + ' ';
+      arrayPhone.push(aux);  
+    }
+    if (index === 7) {
+      let aux = '-';
+      arrayPhone.push(aux);  
+    }
+    arrayPhone.push(arrayNumbers[index]);
+  }
+  return arrayPhone.join('');
 }
 
 // Desafio 12
