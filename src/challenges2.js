9b41b905c9fb48e2bfa23e92127323c6c83e2981
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2021-09-07 00:18:21 -0300

    Adiciona alterações no requisito 10.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,14 @@
 // Desafio 10
 function techList() {
-  // seu código aqui
-}
+  let tech = ["React","Jest","HTML","CSS","JavaScript"]
+  let name = "Rafaela";
+  let nomeTech =[]
+  for(let i = 0; i < tech.length; i += 1){
+  nomeTech.push("{tech: 'React', name: name}")
+  }
+  return nomeTech 
+  }
+techList()
 
 // Desafio 11
 function generatePhoneNumber() {
