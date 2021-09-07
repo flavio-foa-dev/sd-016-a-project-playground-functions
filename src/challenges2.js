9b724416ce04e37e4e6dcfc0df3fe2017d535da7
aparecidag365 <mgegoulart365@gmail.com>
2021-09-07 00:28:31 -0300

    Implemtentado a funçao do desafio 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -23,24 +23,23 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-let fone = generatePhoneNumber([0, 2, 3, 4, 5, 8, 7, 9, 1, 0, 7]);
+//let fone = generatePhoneNumber([0, 2, 3, 4, 5, 8, 7, 9, 1, 0, 7]);
 function generatePhoneNumber(arrayNumber) {
   let arrayNumberNew = arrayNumber.slice(0, 11);
   let phoneNumber = "(";
-  //let sumNumber = 3; 
   if (arrayNumber.length != 11) {
     return "Array com tamanho incorreto."
   } else {
     let sumNumber = repeteasNumber(arrayNumber);
-    for (index = 0; index < arrayNumberNew.length; index += 1){
+    for (index = 0; index < arrayNumberNew.length; index += 1) {
       if ((arrayNumberNew[index] < 0) || (arrayNumberNew[index] > 9) || (sumNumber >= 3)) {
         return "não é possível gerar um número de telefone com esses valores"
       } else {
         if (index === 2) {
-           phoneNumber = phoneNumber + ") "
+          phoneNumber = phoneNumber + ") "
         }
         if (index === 7) {
-           phoneNumber = phoneNumber + "-"
+          phoneNumber = phoneNumber + "-"
         }
         //console.log(arrayNumber[index])
         phoneNumber = phoneNumber + arrayNumberNew[index]
@@ -65,7 +64,7 @@ function repeteasNumber(orderArrayNumber) {
       numberCurrent = orderArrayNumber[indexOrder];
       sumNumber += 1;
     } else {
-      if (sumNumber < 3){
+      if (sumNumber < 3) {
         sumNumber = 1;
       }
       numberCurrent = orderArrayNumber[indexOrder];
@@ -76,8 +75,24 @@ function repeteasNumber(orderArrayNumber) {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+let triangle = triangleCheck(10, 14, 3);
+function triangleCheck(lineA, lineB, lineC) {
+  //Um triângulo é composto de três linhas: `lineA`, `lineB` e `lineC`. 
+  //Crie uma função chamada `triangleCheck` que deverá receber as três linhas como parâmetro e retornar
+  // Para formar um triângulo, a medida de qualquer um dos lados seja menor que a soma das 
+  //medidas dos outros dois e maior que o valor absoluto da diferença entre essas medidas.
+  // - Para obter o valor absoluto de um número em JavaScript, pesquise pela função `Math.abs`.
+  // - O retorno da sua função deverá ser um booleano..
+  // Exemplo: o retorno de `triangleCheck(10, 14, 8)` deverá ser `true`.
+  resultAbsA = Math.abs(lineB - lineC)
+  resultAbsB = Math.abs(lineB - lineC)
+  resultAbsC = Math.abs(lineB - lineC)
+  if ((lineA > resultAbsA) && (lineA < lineB + lineC) && (lineB > resultAbsB) && (lineB < lineA + lineC) && ((lineC > resultAbsC) && (lineC < lineB + lineA))) {
+    //console.log(true)
+    return true
+  } else {
+    //console.log(false)
+  } return false
 }
 
 // Desafio 13
