b029615797b37273e3211838e0120939335f09bf
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-06 21:52:07 -0300

    Resolução do desafio 11 - Bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,9 +14,46 @@ function techList(arrayTechnology, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayPhoneNumber) {
+  let numberPhone;
+  let compareNumber;
+  let numberRepeat = 0;
+  let test1 = 0;
+  let test2 = 0;
+  if (arrayPhoneNumber.length !== 11) {
+    numberPhone = 'Array com tamanho incorreto.';
+  } else {
+    for (let i = 0; i < arrayPhoneNumber.length; i += 1) {
+      numberRepeat = 0;
+      compareNumber = arrayPhoneNumber[i];
+      for (let j = 0; j < arrayPhoneNumber.length; j += 1) {
+        if (compareNumber === arrayPhoneNumber[j]) {
+          numberRepeat += 1;
+          if (numberRepeat >= 3) {
+            test1 = 1;
+            break;
+          }
+        } else if (arrayPhoneNumber[j] < 0 || arrayPhoneNumber[j] > 9) {
+          test2 = 1;
+          break;
+        }
+      }
+      if (test1 === 1 || test2 === 1) {
+        numberPhone = 'não é possível gerar um número de telefone com esses valores';
+        break;
+      } else {
+        numberPhone = '(xx) xxxxx-xxxx';
+        for (i = 0; i < numberPhone.length; i += 1) {
+          numberPhone = numberPhone.replace('x', arrayPhoneNumber[i]);
+        }
+      }
+    }
+  }
+  return numberPhone;
 }
+
+let result = generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2]);
+console.log(result);
 
 // Desafio 12
 function triangleCheck() {
