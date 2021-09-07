3103e573c15128b9d165361c5278946d04b8520f
Laura <lahfumagalli@gmail.com>
2021-09-07 17:46:26 -0300

    11

--- src/challenges2.js
+++ src/challenges2.js
@@ -8,11 +8,11 @@ function techList(arrayTechs, name) {
     return "Vazio!"
   } else {
     arrayTechsSort = arrayTechs.sort();
-    for (index = 0; index < arrayTechs.length; index += 1){
+    for (index = 0; index < arrayTechs.length; index += 1) {
       techs = {
         tech: arrayTechsSort[index],
-        name : name
-        }
+        name: name
+      }
       arrayTechsObject.push(techs);
     }
     return arrayTechsObject;
@@ -20,8 +20,33 @@ function techList(arrayTechs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array11numbers) {
+  if (array11numbers.length > 11 || array11numbers.length < 11) {
+    return "Array com tamanho incorreto."
+  } else {
+    for (let key in array11numbers) {
+      if (array11numbers[key] < 0 || array11numbers[key] > 9) {
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+    let contador = 0;
+    for (let k in array11numbers) {
+      let number = array11numbers[k]
+      for (let n of array11numbers) {
+        if (n === number) {
+          contador += 1
+        }
+        if (contador > 2) {
+          return "não é possível gerar um número de telefone com esses valores";
+        }
+      }
+      contador = 0;
+    }
+    let ddd = array11numbers.slice(0, 2)
+    let celPhoneBegin = array11numbers.slice(2, 7)
+    let celPhoneEnd = array11numbers.slice(7, 11)
+    return "(" + ddd.join("") + ") " + celPhoneBegin.join("") + "-" + celPhoneEnd.join("")
+  }
 }
 
 // Desafio 12
