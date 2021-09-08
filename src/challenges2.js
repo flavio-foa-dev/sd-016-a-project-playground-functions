9711330792ec03b92f4bf40ad74ad762236f6142
Felipe Matheus <fmatheusog@gmail.com>
2021-09-08 11:03:38 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -13,7 +13,43 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function countNumber(array, value) {
+  let count = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === value) count += 1;
+  }
+
+  return count;
+}
+
+function checkCount(numbers) {
+  for (let value of numbers) {
+    if (countNumber(numbers, value) >= 3) return false;
+  }
+}
+
+function checkNumbers(numbers) {
+  for (let value of numbers) {
+    if (value > 9) return false;
+    if (value < 0) return false;
+  }
+  return true;
+}
+
+function generatePhoneNumber(numbers) {
+  let msg = 'não é possível gerar um número de telefone com esses valores';
+  if (numbers.length !== 11) return 'Array com tamanho incorreto.';
+  if (checkNumbers(numbers) === false) return msg;
+  if (checkCount(numbers) === false) return msg;
+
+  let ddd = numbers.slice(0, 2);
+  ddd = ddd.join('');
+  let firstFour = numbers.slice(2, 7);
+  firstFour = firstFour.join('');
+  let lastFour = numbers.slice(7, 11);
+  lastFour = lastFour.join('');
+
+  return `(${ddd}) ${firstFour}-${lastFour}`;
 }
 
 // Desafio 12
