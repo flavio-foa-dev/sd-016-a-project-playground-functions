d3d3784845dc00e8df8f2eb16108fb0f631c6062
baky2708 <flavioisaque123@gmail.com>
2021-09-09 10:54:06 -0300

    10 challenge - Complete

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,24 @@
 // Desafio 10
-function techList() {
+function techList(array, nameForTech) {
   // seu código aqui
+  let objectList = [];
+
+  array.sort();
+
+  for (let i = 0; i < array.length; i += 1) {
+    let a = {
+      tech: array[i],
+      name: nameForTech,
+    };
+    objectList[i] = a;
+  }
+  if (objectList.length === 0) {
+    return 'Vazio!';
+  }
+  return objectList;
 }
+let b = [];
+console.log(techList(b, 'flavio'));
 
 // Desafio 11
 function generatePhoneNumber() {
