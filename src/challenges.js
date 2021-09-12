94b66a33d72417aaeba5f0f4d6e679ab5f45fa78
neguinho <victor_maanoel@Outlook.com>
2021-09-12 01:29:49 -0400

    fechando hj

--- src/challenges.js
+++ src/challenges.js
@@ -4,7 +4,7 @@ function compareTrue(param1, param2) {
   if(param1 === true && param2 === true){
     return true
   } else {
-    return true
+    return false
   }
 }
 // Desafio 2 - FEITO
--- src/challenges2.js
+++ src/challenges2.js
@@ -30,6 +30,7 @@ function generatePhoneNumber(numbers) {
     let lastNumbers = numbers.slice(7,11)
     lastNumbers = lastNumbers.join("")
     return `(${ddd}) ${firstNumbers}-${lastNumbers}`
+    
 }
   console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
