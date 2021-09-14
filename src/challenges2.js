50c1b260c5ee240e5d3214a8ec6c5d4fa2ef33df
PatrickR00t <patrick.s.falquetti@gmail.com>
2021-09-14 15:23:49 -0300

    versão15.1

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,8 +16,31 @@ function techList(arrayTech, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(number) {
+  let counter = 0;
+  let selfFone;
+
+  //checando se tem 11 valores
+  if(numbers.length === 11){
+    for(i = 0; i < number.length; i += 1){
+      if(number[i] < 0 || number[i] > 9){
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+      for(l = 0; l < number.length; l += 1){
+        if(number[l] === number[l]){
+          counter += 1;
+          if(counter >= 3){
+            return "não é possível gerar um número de telefone com esses valores";
+          }
+        }
+      }
+      counter = 0;
+    }
+    selfFone = "(" + numbers[0] + numbers[1] + ")" + " " + numbers[2] + numbers[3] + numbers[4] + numbers[5] +  numbers[6] + "-" + numbers[7]+ numbers[8] + numbers[9] + numbers[10];
+    return selfFone;
+  } else{
+    return "Array com tamanho incorreto.";
+  }
 }
 
 // Desafio 12
