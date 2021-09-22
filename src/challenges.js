6f09dea55fce418d860846b6a97486fc742fb63d
Rafael <rafaelrtssilva@gmail.com>
2021-09-22 01:02:57 -0300

    6,7,8,9

--- src/challenges.js
+++ src/challenges.js
@@ -25,31 +25,87 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return wins*3 +ties;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
-}
+  let maior= array[0];
+  for( let i=0; i<= array.length;i++){
+    if(array[i] > maior)
+      {maior = array[i];}
+    } 
+   let contador = 0;
+   for ( let I=0 ;I<=array.length;I++){
+     if (maior === array[I]){
+       contador ++};
+     }
+     return contador;
+   }
+
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-}
+
+     if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+      return 'cat1';
+    } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+      return 'cat2';
+    } else {
+      return "os gatos trombam e o rato foge";
+    }
+  }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
   // seu código aqui
-}
+  let arrayNovo = [];
+  for(let i=0;i<=array.length;i++){
+    if (array[i] %3 === 0 && array[i] %5 !== 0){
+      arrayNovo.push ("fizz");
+    }
+    else if (array[i] %3 !== 0  && array[i]%5 === 0){
+      arrayNovo.push("buzz");
+    }
+    else if ( array[i] %3 === 0 &&  array[i] %5 === 0){
+      arrayNovo.push("fizzBuzz");
+    } 
+    else {arrayNovo.push("bug!");
+  }}
+    return arrayNovo;
+  }
+  
+
 
 // Desafio 9
-function encode() {
+function encode(string) {
   // seu código aqui
+  
+  let stringNova = string.split('');
+    for (let i=0; i <= stringNova.length; i++){
+    if (stringNova[i]=== "a"){stringNova[i] ="1";} 
+     else if(stringNova[i]==="e") {stringNova[i]="2";}
+     else if(stringNova[i]==="i") {stringNova[i]="3";}
+     else if(stringNova[i]==="o") {stringNova[i]="4";}
+     else if(stringNova[i] ==="u") {stringNova[i]="5";}
+    } 
+    return stringNova.join('');
 }
-function decode() {
+function decode(string) {
   // seu código aqui
+  novaString = string.split('');
+for (let i=0; i <= novaString.length; i++){
+  if (novaString[i]=== "1"){novaString[i] ="a";} 
+   else if(novaString[i]==="2") {novaString[i]="e";}
+   else if(novaString[i]==="3") {novaString[i]="i";}
+   else if(novaString[i]==="4") {novaString[i]="o";}
+   else if(novaString[i] ==="5") {novaString[i]="u";}
+  } 
+  return novaString.join('');
 }
 
 module.exports = {
