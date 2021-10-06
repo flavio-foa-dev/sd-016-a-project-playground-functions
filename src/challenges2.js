528a665c1c8866b3db0b2b12330c64c00f4a8265
Leonardo_Nunes <leonardokarate95@hotmail.com>
2021-10-06 20:57:10 -0300

    implementa a função hydrate

--- src/challenges2.js
+++ src/challenges2.js
@@ -45,11 +45,21 @@ function triangleCheck(linhaA, linhaB, linhaC) {
   return resultado;
 }
 
-// Desafio 13
-function hydrate() {
+function hydrate(string) {
+  let resultado = 0;
+  // eslint-disable-next-line radix
+  let stringSeparada = string.split(' ');
+  for (let i = 0; i < stringSeparada.length; i+=1) {
+    if (stringSeparada[i][0] === '1' || stringSeparada[i][0] === '2' || stringSeparada[i][0] === '3' || stringSeparada[i][0] === '4' || stringSeparada[i][0] === '5' || stringSeparada[i][0] === '6' || stringSeparada[i][0] === '7' || stringSeparada[i][0] === '8' || stringSeparada[i][0] === '9' ) {
+      resultado += parseInt(stringSeparada[i])
+    }
+  }
+  if (resultado === 1){
+    return '1 copo de água'
+  }
+  return `${resultado} copos de água`;
   // seu código aqui
 }
-
 module.exports = {
   generatePhoneNumber,
   techList,
--- teste1.js
+++ teste1.js
@@ -1,10 +1,1 @@
-function techList(array, nome) {
-  let resultado = [];
-  array.sort();
-  for (let i = 0; i < array.length; i += 1) {
-    resultado.push({ tech: array[i], name: nome });
-  }
-  return resultado;
-}
-let a = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
-console.log(techList(a, 'lucas'));
+console.log(typeof parseInt('4'));
