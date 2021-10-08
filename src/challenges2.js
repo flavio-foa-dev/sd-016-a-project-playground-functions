dbd53d9ba9831b5bac42af1e7804bdf1e721e8d9
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-10-08 00:28:01 -0300

    Requisito 12 completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,13 +17,16 @@ function generatePhoneNumber(num) {
   if (num.some((value) => value < 0)) return 'não é possível gerar um número de telefone com esses valores'
   return `(${num[0]}${num[1]}) ${num[2]}${num[3]}${num[4]}${num[5]}${num[6]}-${num[7]}${num[8]}${num[9]}${num[10]}`;
 }
-const numbers = [1,2,3,4,5,6,7,8,9,0,1];
-console.log(generatePhoneNumber(numbers));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if ((lineA < lineB+lineC)&&(lineB < lineA+lineC)&&(lineC < lineA+lineB)&&(lineA > Math.abs(lineB - lineC))&&(lineB > Math.abs(lineA - lineC))&&(lineC > Math.abs(lineA - lineB))){
+    return true;
+  }
+  return false;
 }
+
+console.log(triangleCheck(10, 14, 8));
 
 // Desafio 13
 function hydrate() {
