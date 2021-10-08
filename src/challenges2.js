654782ff3134bd89ec805561309e706c1f1090e1
João Vitor Spala <joaovitorspala.js@gmail.com>
2021-10-08 01:03:10 -0300

    Requisito 12 corrigido

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,7 +14,21 @@ function techList(techs, name) {
 // Desafio 11
 function generatePhoneNumber(num) {
   if (num.length !== 11) return 'Array com tamanho incorreto.';
-  if (num.some((value) => value < 0)) return 'não é possível gerar um número de telefone com esses valores'
+  if (num.some((value) => value < 0) || num.some((value) => value > 9)) return 'não é possível gerar um número de telefone com esses valores';
+    else {
+      isValid = true;
+      for (let index1 = 0; index1 < num.length; index1 += 1) {
+        let count = 0;
+        for (let index2 = 1; index2 < num.length; index2 += 1) {
+          if (num[index1] === num[index2]) count++;
+          if (count >= 3) {
+            isValid = false;
+            break;
+          }
+        }
+      }
+      if (isValid === false) return 'não é possível gerar um número de telefone com esses valores';
+    }
   return `(${num[0]}${num[1]}) ${num[2]}${num[3]}${num[4]}${num[5]}${num[6]}-${num[7]}${num[8]}${num[9]}${num[10]}`;
 }
 
