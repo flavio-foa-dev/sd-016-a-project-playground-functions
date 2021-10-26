ef32104f697f3de0e8943ce031eed859d43fe5b8
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-10-26 00:27:27 -0300

    tentativa frustrada de fatoraçao

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,27 +1,24 @@
 // Desafio 10
 function techList(techs, name) {
-  let techsSorted = techs.sort();
-  let array = [];
+	let techsSorted = techs.sort();
+	let array = [];
   if (techs.length === 0) {
-   return 'Vazio!'
-  } else {
-  for (let index = 0; index < techsSorted.length; index += 1) { 
-  let elemento = {
-  tech: techsSorted[index],
-  name: name
-  }
-  techsSorted[index] === elemento.tech;
-  array.push(elemento)
-  }
-  }
-  return array
-  }
+		return 'Vazio!';
+	} {
+		for (let index = 0; index < techsSorted.length; index += 1) {
+			let elemento = {
+				tech: techsSorted[index],
+				name: name,
+			};
+			array.push(elemento);
+		}
+	};
+	return array;
+};
   
-  let arrayTecnologia2 = ["React", "Jest", "HTML", "CSS", "JavaScript"];
-  console.log(techList(arrayTecnologia2, 'Lucas'));
-  
-  let arrayTecnologia = [];
-  console.log(techList(arrayTecnologia, 'Lucas'));
+let arrayTecnologia2 = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+console.log(techList(arrayTecnologia2, 'Lucas'));
+let arrayTecnologia = [];console.log(techList(arrayTecnologia, 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
@@ -30,22 +27,22 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lado1, lado2, lado3) {
-  let resposta1 = false;
-
-  if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2 && lado1 > Math.abs(lado2 - lado3) && lado2 > Math.abs(lado1 - lado3) && lado3 > Math.abs(lado1 - lado2)) {
-    resposta1 = true;
-  } else {
-    resposta1 = false;
-  }
-  return resposta1;
+	let condição1 = lado1 < lado2 + lado3 && 
+		lado2 < lado1 + lado3 && lado3 < lado1 + lado2;
+	let condição2 = lado1 > Math.abs(lado2 - lado3) && 
+		lado2 > Math.abs(lado1 - lado3) && lado3 > Math.abs(lado1 - lado2);
+	if (condição1 === true && condição2 === true) {
+		return true
+	} return false;
 }
 console.log(triangleCheck(10, 14, 8));
 console.log(triangleCheck(2, 3, 4));
 console.log(triangleCheck(16, 20, 30));
 
-
 // Desafio 13
 function hydrate(string) {
+	// uso do regex foi tirado da seguinte fonte
+	// https://stackoverflow.com/questions/2555059/javascript-regular-expressions-replace-non-numeric-characters
   let contagem = string.match(/[1-9]+/g);
   let contadorBedidas = 0;
 
@@ -58,7 +55,7 @@ function hydrate(string) {
     return contadorBedidas + ' copos de água';
 }
 
-console.log(hydrate("1 cerveja"));
+console.log(hydrate('1 cerveja'));
 console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 console.log(hydrate('2 shots de tequila, 2 cervejas e 1 corote'));
 console.log(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho'));
